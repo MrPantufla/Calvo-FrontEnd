f8fd5741528c0d3bf1d8c0388c56e7e910cc5556
MrPantufla <juanmapizzioo@gmail.com>
2023-11-02 16:11:01 -0300

    Responsive carousel landscape

--- src/Principal/Body/Carrusel/carrusel.jsx
+++ src/Principal/Body/Carrusel/carrusel.jsx
@@ -2,39 +2,34 @@ import './carrusel.css';
 import carrusel1 from '../../../Imagenes/carrusel1.jpg';
 import carrusel2 from '../../../Imagenes/carrusel2.jpg';
 import carrusel3 from '../../../Imagenes/carrusel3.jpg';
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function Carrusel() {
   return (
-    <div className="contenedorPrincipalCarrusel">
-      <div id="carouselExampleIndicators" className="carousel slide">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={carrusel1} className="d-block" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src={carrusel2} className="d-block" alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src={carrusel3} className="d-block" alt="..." />
-          </div>
-        </div>
-
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" className="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-            <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
-          </svg>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" className="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-            <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z" />
-          </svg>
-        </button>
-      </div>
-    </div>
-  );
-}
+    <div className="container">
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={carrusel1}
+          alt="Primer slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={carrusel1}
+          alt="Segundo slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={carrusel1}
+          alt="Tercer slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  </div>
+);
+  }
