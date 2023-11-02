d03bfe9998c78eea6f2091567bc61f98d43d970c
MrPantufla <juanmapizzioo@gmail.com>
2023-11-02 11:57:44 -0300

    Seccion nuestros productos

--- src/Principal/Body/Categorias/nuestrosProductos.jsx
+++ src/Principal/Body/Categorias/nuestrosProductos.jsx
@@ -1,9 +1,11 @@
-import 'nuestrosProductos.css';
+import './nuestrosProductos.css';
 
 export default function NuestrosProductos(){
     return(
         <div className="contenedorPrincipalNuestrosProductos">
-            
+            <div className="contenedorh1">
+                <h1 className="textoNuestrosProductos">NUESTROS PRODUCTOS</h1>
+            </div>
         </div>
     );
 }
--- src/Principal/Body/body.jsx
+++ src/Principal/Body/body.jsx
@@ -1,11 +1,13 @@
 import Carrusel from './Carrusel/carrusel.jsx';
 import Categorias from './Categorias/categorias.jsx';
+import NuestrosProductos from './Categorias/nuestrosProductos.jsx';
 import './body.css';
 
 export default function Body(){
     return(
         <div className="contenedorPrincipalBody">
             <Carrusel/>
+            <NuestrosProductos/>
             <Categorias/>
         </div>
         
