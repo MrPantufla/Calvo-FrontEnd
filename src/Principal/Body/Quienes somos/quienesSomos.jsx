701883d97c2b72709643078ef07cea0d0fdc326d
MrPantufla <juanmapizzioo@gmail.com>
2023-11-03 18:52:34 -0300

    Seteo quienes somos

--- src/Principal/Body/Quienes somos/quienesSomos.jsx
+++ src/Principal/Body/Quienes somos/quienesSomos.jsx
@@ -3,6 +3,16 @@ import './quienesSomos.css';
 export default function QuienesSomos(){
     return(
         <div className="contenedorPrincipalQuienesSomos">
+            <div className="textoQuienesSomos">
+                <div className="tituloYPoligono">
+                    <div className="poligono"/>
+                    <h2>QUIÉNES SOMOS</h2>
+                </div>
+                <p className="parrafo"></p>
+            </div>
+            <div className="contenedorImagen">
+                <img src="" className="imagenQuienesSomos"/>
+            </div>
         </div>
     );
 }
