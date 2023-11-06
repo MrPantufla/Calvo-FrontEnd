6e574ef834e89eec65aae98d7ff3dc73d7d7e317
MrPantufla <juanmapizzioo@gmail.com>
2023-11-06 11:45:05 -0300

    Enlaces a telefonos y css carousel

--- src/Principal/Body/Carrusel/carrusel.jsx
+++ src/Principal/Body/Carrusel/carrusel.jsx
@@ -7,29 +7,29 @@ import Carousel from 'react-bootstrap/Carousel';
 export default function Carrusel() {
   return (
     <div className="container">
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={carrusel1}
-          alt="Primer slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={carrusel1}
-          alt="Segundo slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={carrusel1}
-          alt="Tercer slide"
-        />
-      </Carousel.Item>
-    </Carousel>
-  </div>
-);
-  }
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={carrusel1}
+            alt="Primer slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={carrusel1}
+            alt="Segundo slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={carrusel1}
+            alt="Tercer slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+}
