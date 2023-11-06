b2c0a6a8c6484fd78a7257239f07e77ea1c1600d
MrPantufla <juanmapizzioo@gmail.com>
2023-11-06 10:17:40 -0300

    CSS icono perfil

--- src/Principal/Header/header.jsx
+++ src/Principal/Header/header.jsx
@@ -75,9 +75,11 @@ export default function Header() {
             <p>CONTACTO</p>
           </a>
           <a className="perfil">
+            <div className="iconoContainer">
             <svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
               <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
             </svg>
+            </div>
           </a>
         </div>
       </div>
