2ec73622dc258851306f6ebcf9c54e6c2ce90573
MrPantufla <juanmapizzioo@gmail.com>
2023-11-07 07:05:31 -0300

    className y fillRule

--- src/Principal/Header/header.jsx
+++ src/Principal/Header/header.jsx
@@ -81,8 +81,8 @@ export default function Header() {
 
   return (
     <div className="container-fluid px-0 contenedorPrincipalHeader" id="header" style={headerStyle}>
-      <button className="botonHeader" id="toggleHeaderButton" style={{ display: window.innerWidth < 768 ? 'block' : 'none' }} onClick={toggleHeader}><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+      <button className="botonHeader" id="toggleHeaderButton" style={{ display: window.innerWidth < 768 ? 'block' : 'none' }} onClick={toggleHeader}><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+        <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
       </svg>
       </button>
       <Collapse className="headerVisible" in={headerAbierto}>
