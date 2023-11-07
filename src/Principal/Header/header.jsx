1d18b438d6bf61d63a2caf1de9f8fdb1a5764031
MrPantufla <juanmapizzioo@gmail.com>
2023-11-07 10:46:39 -0300

    CSS mobile header

--- src/Principal/Header/header.jsx
+++ src/Principal/Header/header.jsx
@@ -84,14 +84,14 @@ export default function Header() {
 
   return (
     <div className="container-fluid px-0 contenedorPrincipalHeader" id="header" style={headerStyle}>
-      <div className="headerMobile" style={{ display: mobile ? 'flex' : 'none' }}>
-        <div className="containerBotonMobile">
+      <div className="containerBotonMobile">
           <button className="botonHeader" id="toggleHeaderButton" onClick={toggleHeader}>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
               <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
             </svg>
           </button>
         </div>
+      <div className="headerMobile" style={{ display: mobile ? 'flex' : 'none' }}>
         <div className="containerLogoMobile">
           <img src={logo} className="logoHeaderMobile" />
         </div>
