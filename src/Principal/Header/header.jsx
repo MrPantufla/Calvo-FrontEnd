1bc774bb865e36651409ef228497fe84b67994eb
MrPantufla <juanmapizzioo@gmail.com>
2023-11-07 11:40:46 -0300

    Display none a no mobile

--- src/Principal/Header/header.jsx
+++ src/Principal/Header/header.jsx
@@ -85,7 +85,7 @@ export default function Header() {
   return (
     <div className="container-fluid px-0 contenedorPrincipalHeader" id="header" style={headerStyle}>
       <div className="containerBotonMobile">
-          <button className="botonHeader" id="toggleHeaderButton" onClick={toggleHeader}>
+          <button className="botonHeader" id="toggleHeaderButton" onClick={toggleHeader} style={{ display: mobile ? 'flex' : 'none' }}>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
               <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
             </svg>
