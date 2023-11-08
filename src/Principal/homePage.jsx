d6ed23cd774a3c94d6213dc48816b35df53636b6
MrPantufla <juanmapizzioo@gmail.com>
2023-11-08 16:28:16 -0300

    Cambio de import

--- src/Principal/homePage.jsx
+++ src/Principal/homePage.jsx
@@ -1,8 +1,8 @@
 import Header from './Header/header.jsx';
 import Body from './Body/body.jsx';
 import Footer from './Footer/footer.jsx';
-import InformacionYContacto from './Body/Informacion y contacto/informacionYContacto.jsx';
 import HeaderMobile from './Header/headerMobile.jsx';
+import InformacionYContacto from './Body/Informacion y contacto/informacionYContacto.jsx';
 import './homePage.css';
 
 export default function HomePage(){
