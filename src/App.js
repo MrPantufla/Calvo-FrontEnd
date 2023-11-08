cecfb78d24ed703302902dcfd75cf60f2a1208e5
MrPantufla <juanmapizzioo@gmail.com>
2023-11-08 16:29:12 -0300

    Agregado firebase y hosteado

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/home" element={<HomePage />} />
+      <Route path="/home" element={<HomePage/>} />
       <Route path="/tienda" element={<Ventas/>}/>
       <Route path="login" element={<Login/>}/>
       </Routes>
---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKpNbxQqm7qYGhS6SCy0Xd0quhmFx_J1o",
+  authDomain: "frontend-calvo.firebaseapp.com",
+  projectId: "frontend-calvo",
+  storageBucket: "frontend-calvo.appspot.com",
+  messagingSenderId: "372587731953",
+  appId: "1:372587731953:web:413f29b36f035373610250"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
