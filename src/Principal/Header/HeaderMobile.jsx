d2bd1e6ca326458fcc06b3bbaa15d3b349cbda29
MrPantufla <juanmapizzioo@gmail.com>
2023-11-08 11:43:30 -0300

    Header mobile nuevo

---
+++ src/Principal/Header/HeaderMobile.jsx
@@ -0,0 +1,7 @@
+<div className="containerBotonMobile">
+        <button className="botonHeader" id="toggleHeaderButton" onClick={toggleHeader} style={{ display: mobile ? 'flex' : 'none' }}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+          </svg>
+        </button>
+</div>
