a14a8f9cb4890b6113c8b88b3873e577027b4012
MrPantufla <juanmapizzioo@gmail.com>
2023-11-13 09:46:31 -0300

    Boton ultima pagina

--- src/Ventas/FiltrosYProductos/filtrosYProductos.jsx
+++ src/Ventas/FiltrosYProductos/filtrosYProductos.jsx
@@ -114,7 +114,7 @@ export default function FiltrosYProductos(props) {
       </div>
       <div className="paginacion">
         <button 
-          className="botonAntSig button primeraPagina"
+          className="botonAntSig button paginaExtremo"
           onClick={() => paginar(1)}
           disabled={paginaActual === 1}
           >
@@ -179,7 +179,15 @@ export default function FiltrosYProductos(props) {
               d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
             />
           </svg>
-
+        </button>
+        <button 
+          className="botonAntSig button paginaExtremo ultimaPagina"
+          onClick={() => paginar(totalPaginas)}
+          disabled={paginaActual === totalPaginas}
+          >
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z" />
+          </svg>
         </button>
       </div>
     </div>
