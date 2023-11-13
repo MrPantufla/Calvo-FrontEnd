c96678230d3bb4f00fab0a7c407d26005fc4dbb5
MrPantufla <juanmapizzioo@gmail.com>
2023-11-13 17:39:30 -0300

    .

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCKpNbxQqm7qYGhS6SCy0Xd0quhmFx_J1o",
   authDomain: "frontend-calvo.firebaseapp.com",
