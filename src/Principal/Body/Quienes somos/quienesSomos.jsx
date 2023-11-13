8c059aa441f571c1cdd78736f7931b9dcac5ed21
MrPantufla <juanmapizzioo@gmail.com>
2023-11-13 16:13:23 -0300

    Quienes somos escritorio

--- src/Principal/Body/Quienes somos/quienesSomos.jsx
+++ src/Principal/Body/Quienes somos/quienesSomos.jsx
@@ -1,5 +1,4 @@
 import './quienesSomos.css';
-import Fondo from '../../../Imagenes/fondoQuienesSomos.jpg';
 import imagenQuienesSomos from '../../../Imagenes/imagenDeposito.png';
 import CAquienesSomos from '../../../Imagenes/CAquienesSomos.png';
 
@@ -7,16 +6,16 @@ export default function QuienesSomos() {
 
     const message = "Fundada en 1994, somos una empresa con amplia trayectoria que se ha destacado siempre por su enfoque en el servicio al cliente. \nEntre nuestros valores destacan la honestidad, confianza, responsabilidad y compromiso que ofrecemos tanto dentro del ámbito laboral como con nuestros proveedores y clientes. \n Nos encargamos de la comercialización y distribución de insumos para la carpintería de aluminio y contamos con más de 400 clientes a lo largo de las provincias de Entre Ríos, Corrientes y Misiones, a los que con un sistema de transporte propio, les somos garantía de una calidad de servicio y productos sobresaliente.";
 
-    const backgroundStyle = {
-        backgroundImage: `url(${Fondo})`,
-        backgroundSize: '80%',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-    };
-
     return (
         <div className="contenedorPrincipalQuienesSomos">
-            <div className="fondoYTextoContainer" style={backgroundStyle}>
+            <div className="fondoContainer">
+                <div className="logoQuienesSomosContainer">
+                    <img className="logoQuienesSomos" src={CAquienesSomos}/>
+                </div>
+                <div className="imagenQuienesSomosContainer">
+                    <img className="imagenQuienesSomos" src={imagenQuienesSomos}/>
+                </div>
+            </div>
             <div className="textoQuienesSomos">
                     <div className="tituloYPoligono">
                         <div className="poligono" />
@@ -28,7 +27,7 @@ export default function QuienesSomos() {
                         ))}
                     </div>
                 </div>
-            </div> 
+             
         </div>
     );
 }
