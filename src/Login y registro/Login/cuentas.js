eb650648890b8a72d7e4fcf5b59b99164a4ec844
MrPantufla <juanmapizzioo@gmail.com>
2023-11-15 18:02:00 -0300

    .

---
+++ src/Login y registro/Login/cuentas.js
@@ -0,0 +1,22 @@
+export const cuentas = [
+    {
+        email: "usuario1",
+        password: "contraseña1"
+    },
+    {
+        email: "usuario2",
+        password: "contraseña2"
+    },
+    {
+        email: "usuario3",
+        password: "contraseña3"
+    },
+    {
+        email: "usuario4",
+        password: "contraseña4"
+    },
+    {
+        email: "usuario5",
+        password: "contraseña5"
+    }
+]
