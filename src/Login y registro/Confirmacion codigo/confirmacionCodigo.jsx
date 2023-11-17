8c3333f5e9ef8122888996de8d82f6f0ee187799
MrPantufla <juanmapizzioo@gmail.com>
2023-11-17 18:52:41 -0300

    Confirmacion codigo

--- src/Login y registro/Confirmacion codigo/confirmacionCodigo.jsx
+++ src/Login y registro/Confirmacion codigo/confirmacionCodigo.jsx
@@ -1,46 +1,58 @@
 import './confirmacionCodigo.css';
 
 import { useState } from 'react';
+import { useAuth } from '../../contextLogin';
 
 export default function ConfirmacionCodigo() {
-    const [codigoConfirmacion, setCodigoConfirmacion] = useState('');
+  const [codigoConfirmacion, setCodigoConfirmacion] = useState('');
+  const [mensajeRespuesta, setMensajeRespuesta] = useState('');
+  const { state: userData, updateEmailConfirmationStatus } = useAuth();
 
-    const enviarCodigo = () => {
-        fetch('http://localhost:8080/api/confirmacionCodigo', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ codigoConfirmacion }),
-          credentials: 'include',
-        })
-          .then(response => response.text())
-          .then(data => {
-            console.log('Respuesta: ', data);
-            // Aquí puedes manejar la respuesta del backend, por ejemplo, mostrar un mensaje al usuario
-          })
-          .catch(error => {
-            console.error('Ocurrió un error al enviar el código:', error);
-          });
-      };
-      
+  const enviarCodigo = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/confirmacionCodigo', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          codigoConfirmacion,
+          email: userData.userInfo.email,
+        }),
+      });
 
-    return (
-        <div className="contenedorPrincipalConfirmacionCodigo">
-            <form>
-                <div className="form-group">
-                    <label htmlFor="codigo" required></label>
-                    <input
-                        type="text"
-                        id="codigo"
-                        value={codigoConfirmacion}
-                        onChange={(e) => setCodigoConfirmacion(e.target.value)}
-                    />
-                </div>
-                <button type="button" onClick={enviarCodigo}>
-                    Enviar código
-                </button>
-            </form>
+      if (response.ok) {
+        const data = await response.text();
+        setMensajeRespuesta(data.message);
+        updateEmailConfirmationStatus();
+        
+      } else {
+        const data = await response.text();
+        setMensajeRespuesta(`Error: ${data.error}`);
+      }
+    } catch (error) {
+      console.error('Error al enviar el código:', error);
+      setMensajeRespuesta('Error al enviar el código');
+    }
+  };
+
+
+  return (
+    <div className="contenedorPrincipalConfirmacionCodigo">
+      <form>
+        <div className="form-group">
+          <label htmlFor="codigo" required></label>
+          <input
+            type="text"
+            id="codigo"
+            value={codigoConfirmacion}
+            onChange={(e) => setCodigoConfirmacion(e.target.value)}
+          />
         </div>
-    );
+        <button type="button" onClick={enviarCodigo}>
+          Enviar código
+        </button>
+      </form>
+    </div>
+  );
 }
