ed9b34cf3bbd981d3e79fe18a234dadb43946b13
MrPantufla <juanmapizzioo@gmail.com>
2023-11-23 17:50:36 -0300

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import HomePage from './Principal/homePage';
 import Ventas from './Ventas/ventas.jsx';
 import Perfil from './Perfil/perfil.jsx';
-import LoginYRegistro from './Login y registro/loginYRegistro.jsx';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { LoginProvider } from './contextLogin';
 
@@ -15,7 +14,6 @@ function App() {
       <Routes>
       <Route path="/home" element={<HomePage/>} />
       <Route path="/tienda" element={<Ventas/>}/>
-      <Route path="/login" element={<LoginYRegistro/>}/>
       <Route path="/perfil" element={<Perfil/>}/>
       </Routes>
     </Router>
