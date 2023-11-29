ae97941356b5e01451729c1d783ad983a2bae4a6
MrPantufla <juanmapizzioo@gmail.com>
2023-11-29 01:38:06 -0300

    Descarga de archivos

--- src/Ventas/Catalogos/catalogos.jsx
+++ src/Ventas/Catalogos/catalogos.jsx
@@ -4,16 +4,24 @@ import { useDesplegable } from '../../contextDesplegable';
 export default function Catalogos() {
     const desplegable = useDesplegable();
 
+    const handleDownload = (filename) => {
+        const filePath = `${process.env.PUBLIC_URL}/Archivos/${filename}.pdf`;
+        const link = document.createElement('a');
+        link.href = process.env.PUBLIC_URL + filePath;
+        link.download = `${filename}.pdf`;
+        link.click();
+    };
+
     return (
         <div
-            className={`desplegableCatalogos ${desplegable.hovered ? 'open' : ''}`}
+            className={`desplegableCatalogos ${desplegable.hovered ? 'open' : 'open'}`}
             onMouseEnter={desplegable.abrirHover}
             onMouseLeave={desplegable.cerrarHover}
         >
             <div className="descargarCatalogosContainer">
-                <p>PERFILES</p>
-                <p>ACCESORIOS</p>
-                <p>HERRAMIENTAS</p>
+                <a onClick={() => handleDownload('perfiles')}>PERFILES</a>
+                <a onClick={() => handleDownload('accesorios')}>ACCESORIOS</a>
+                <a onClick={() => handleDownload('herramientas')}>HERRAMIENTAS</a>
             </div>
         </div>
     );
