c309e0e40a9cb9522dadaed601073f1bcf78a4e6
MrPantufla <juanmapizzioo@gmail.com>
2023-11-29 16:29:22 -0300

    Posicion dinamica de desplegable

--- src/Ventas/Catalogos/catalogos.jsx
+++ src/Ventas/Catalogos/catalogos.jsx
@@ -1,8 +1,10 @@
 import './catalogos.css';
 import { useDesplegable } from '../../contextDesplegable';
+import { useState, useEffect } from 'react';
 
 export default function Catalogos() {
     const desplegable = useDesplegable();
+    const [catalogosTop, setCatalogosTop] = useState(8.7);
 
     const handleDownload = (filename) => {
         const filePath = `${process.env.PUBLIC_URL}/Archivos/${filename}.pdf`;
@@ -12,11 +14,40 @@ export default function Catalogos() {
         link.click();
     };
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const maxCatalogosTop = 8.7; // ajusta el valor máximo de altura según tus necesidades
+            const minCatalogosTop = 6.2; // ajusta el valor mínimo de altura según tus necesidades
+            const alturaHeader = 150; // ajusta según tus necesidades
+    
+            // Calcula la nueva posición top en función del scroll
+            let newTop =
+                maxCatalogosTop -
+                (maxCatalogosTop - minCatalogosTop) * (scrollPosition / alturaHeader);
+    
+            // Asegúrate de que newTop no sea menor que el valor mínimo de 7
+            newTop = Math.max(minCatalogosTop, newTop);
+    
+            // Establece la nueva posición top
+            setCatalogosTop(newTop);
+        };
+    
+        // Agrega el event listener para el scroll
+        window.addEventListener('scroll', handleScroll);
+    
+        // Limpia el event listener al desmontar el componente
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div
             className={`desplegableCatalogos ${desplegable.hovered ? 'open' : ''}`}
             onMouseEnter={desplegable.abrirHover}
             onMouseLeave={desplegable.cerrarHover}
+            style={{ top: `${catalogosTop}rem` }}
         >
             <div className="descargarCatalogosContainer">
                 <a onClick={() => handleDownload('perfiles')}>PERFILES</a>
