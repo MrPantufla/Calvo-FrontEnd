3b54a4d3c5072a71dfa3d7504aca18014d074998
MrPantufla <juanmapizzioo@gmail.com>
2023-12-01 00:58:55 -0300

    EditarDatos agregado

--- src/Perfil/Body Perfil/bodyPerfil.jsx
+++ src/Perfil/Body Perfil/bodyPerfil.jsx
@@ -1,13 +1,17 @@
 import { useAuth } from '../../contextLogin.jsx';
 import './bodyPerfil.css';
 import Configuracion from './Configuracion/configuracion.jsx';
+import EditarDatos from './Configuracion/editarDatos.jsx';
+import { useEditarDatos } from '../../contextEditarDatos.jsx';
 
 export default function BodyPerfil(){
     const auth = useAuth();
+    const editarDatos = useEditarDatos();
 
     return(
         <div className="contenedorPrincipalBodyPerfil">
             <Configuracion/>
+            {editarDatos.editarDatosAbierto ? (<EditarDatos/>) : (<div/>) }
             <h1>Hola, {auth.state.userInfo.nombre}</h1>
         </div>
     );
