4983c8fb7a6b224b9a31d567ce1de8477662ded2
MrPantufla <juanmapizzioo@gmail.com>
2023-12-01 00:59:22 -0300

    Cambio de datos

--- src/Perfil/Body Perfil/Configuracion/configuracion.jsx
+++ src/Perfil/Body Perfil/Configuracion/configuracion.jsx
@@ -1,18 +1,20 @@
 import { useDesplegableConfiguracion } from '../../../contextDesplegableConfiguracion';
+import { useEditarDatos } from '../../../contextEditarDatos';
 import './configuracion.css';
 
 export default function Configuracion() {
     const desplegable = useDesplegableConfiguracion();
+    const editarDatos = useEditarDatos();
 
     return (
         <div
-            className={`desplegableConfiguracion ${desplegable.hovered ? 'open' : 'open'}`}
+            className={`desplegableConfiguracion ${desplegable.hovered ? 'open' : ''}`}
             onMouseEnter={desplegable.abrirHover}
             onMouseLeave={desplegable.cerrarHover}
         >
             <div className="configuracionContainer">
-                <a>PERFILES</a>
-                <a>ACCESORIOS</a>
+                <a onClick={editarDatos.abrirEditarDatos}>DATOS DE CUENTA</a>
+                <a>MIS DIRECCIONES</a>
                 <a>HERRAMIENTAS</a>
             </div>
         </div>
