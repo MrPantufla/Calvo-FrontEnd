0ebb0653a8f9b40a451c50ad189038fd72dd257a
MrPantufla <juanmapizzioo@gmail.com>
2023-12-04 17:47:34 -0300

    Añadido loginYRegistro

--- src/Perfil/perfil.jsx
+++ src/Perfil/perfil.jsx
@@ -1,11 +1,13 @@
 import Header from '../Principal/Header/header';
 import BodyPerfil from './Body Perfil/bodyPerfil';
 import Footer from '../Principal/Footer/footer.jsx';
+import LoginYRegistro from '../Login y registro/loginYRegistro.jsx';
 
 export default function Perfil(){
     return(
         <div className="contenedorPrincipalPerfil">
             <Header/>
+            <LoginYRegistro/>
             <BodyPerfil/>
             <Footer/>
         </div>
