25ab3af975af1ec36d6a322ece9a0a99d5e71b3a
MrPantufla <juanmapizzioo@gmail.com>
2023-12-05 00:46:06 -0300

    Añadido editarContraseña

--- src/Perfil/Body Perfil/bodyPerfil.jsx
+++ src/Perfil/Body Perfil/bodyPerfil.jsx
@@ -3,15 +3,19 @@ import './bodyPerfil.css';
 import Configuracion from './Configuracion/configuracion.jsx';
 import EditarDatos from './Configuracion/Editar Datos/editarDatos.jsx';
 import { useEditarDatos } from '../../contextEditarDatos.jsx';
+import EditarContraseña from './Configuracion/Editar Contraseña/editarContraseña.jsx';
+import { useEditarContraseña } from '../../contextEditarContraseña.jsx';
 
 export default function BodyPerfil(){
     const auth = useAuth();
     const editarDatos = useEditarDatos();
+    const editarContraseña = useEditarContraseña();
 
     return(
         <div className="contenedorPrincipalBodyPerfil">
             <Configuracion/>
             {editarDatos.editarDatosAbierto ? (<EditarDatos/>) : (<div/>) }
+            {editarContraseña.editarContraseñaAbierto ? (<EditarContraseña/>) : (<div/>)}
             <h1>Hola, {auth.state.userInfo.nombre}</h1>
         </div>
     );
