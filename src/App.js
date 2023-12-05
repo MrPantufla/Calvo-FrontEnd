fca1b006499c3bae710ee6e9c63a256ac4956e92
MrPantufla <juanmapizzioo@gmail.com>
2023-12-05 00:45:44 -0300

    Añadido context editarContraseña

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { LoginProvider } from './contextLogin';
 import { DesplegableProviderCatalogos } from './contextDesplegableCatalogos.jsx';
 import { DesplegableProviderConfiguracion } from './contextDesplegableConfiguracion.jsx';
 import { EditarDatosProvider } from './contextEditarDatos.jsx';
+import { EditarContraseñaContext, EditarContraseñaProvider } from './contextEditarContraseña.jsx';
 
 function App() {
   const apiUrl = "http://localhost:8080/api";
@@ -15,17 +16,17 @@ function App() {
     <DesplegableProviderConfiguracion>
       <DesplegableProviderCatalogos>
         <EditarDatosProvider>
-          <LoginProvider>
-            <Router>
-              <Routes>
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/tienda" element={<Ventas />} />
-
-                <Route path="/perfil" element={<Perfil />} />
-
-              </Routes>
-            </Router>
-          </LoginProvider>
+          <EditarContraseñaProvider>
+            <LoginProvider>
+              <Router>
+                <Routes>
+                  <Route path="/home" element={<HomePage />} />
+                  <Route path="/tienda" element={<Ventas />} />
+                  <Route path="/perfil" element={<Perfil />} />
+                </Routes>
+              </Router>
+            </LoginProvider>
+          </EditarContraseñaProvider>
         </EditarDatosProvider>
       </DesplegableProviderCatalogos>
     </DesplegableProviderConfiguracion>
