2e652fee5a76dc306b1c25267f35ff6769c5c963
MrPantufla <juanmapizzioo@gmail.com>
2023-12-05 21:45:05 -0300

    Error sin solucionar

--- src/Login y registro/Registro/registro.jsx
+++ src/Login y registro/Registro/registro.jsx
@@ -1,6 +1,7 @@
 import './registro.css';
 import { useState } from 'react';
 import { autoLogin } from '../Login/login';
+import { useAuth } from '../../contextLogin';
 
 export default function Registro() {
     const [nombre, setNombre] = useState('');
@@ -12,6 +13,8 @@ export default function Registro() {
     const [confirmContrasenia, setConfirmContrasenia] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
 
+    const auth = useAuth();
+
     const handleRegistro = () => {
         // Expresión regular para validar el formato del correo electrónico
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -19,15 +22,20 @@ export default function Registro() {
 
         if (!nombre || !apellido || !email || !contrasenia || !confirmContrasenia || !cuit || !telefono) {
             setErrorMessage('Por favor, complete todos los campos.');
+            auth.setMostrarError(true);
+            console.log(errorMessage);
             return;
         } else if (!emailRegex.test(email)) {
             setErrorMessage('Ingrese un formato de correo electrónico válido.');
+            auth.setMostrarError(true);
             return;
         } else if (!telefonoRegex.test(telefono)){
             setErrorMessage('En el campo de teléfono solo ingrese números');
+            auth.setMostrarError(true);
             return;
         } else if (contrasenia !== confirmContrasenia) {
             setErrorMessage('Las contraseñas no coinciden.');
+            auth.setMostrarError(true);
             return;
         } else {
             console.log('Registro exitoso');
@@ -81,70 +89,75 @@ export default function Registro() {
 
     return (
         <div className="registro-container">
-            <h2>Registro</h2>
-            <div className="error-message">{errorMessage}</div>
+            <div className="error-message">{auth.mostrarError ? errorMessage : ""}</div>
             <form>
                 <div className="form-group-registro">
-                    <label htmlFor="nombre" required>Nombre/s:</label>
+                    <label htmlFor="nombre" required>Nombre/s</label>
                     <input
                         type="text"
                         id="nombre"
                         value={nombre}
                         onChange={(e) => setNombre(e.target.value)}
+                        onFocus={auth.setMostrarError(false)}
                     />
                 </div>
                 <div className="form-group-registro">
-                    <label htmlFor="apellido" required>Apellido:</label>
+                    <label htmlFor="apellido" required>Apellido</label>
                     <input
                         type="text"
                         id="apellido"
                         value={apellido}
                         onChange={(e) => setApellido(e.target.value)}
+                        onFocus={auth.setMostrarError(false)}
                     />
                 </div>
                 <div className="form-group-registro">
-                    <label htmlFor="emailRegistro" required>Correo Electrónico:</label>
+                    <label htmlFor="emailRegistro" required>Correo Electrónico</label>
                     <input
                         type="emailRegistro"
                         id="emailRegistro"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
+                        onFocus={auth.setMostrarError(false)}
                     />
                 </div>
                 <div className="form-group-registro">
-                    <label htmlFor="telefonoRegistro" required>Teléfono:</label>
+                    <label htmlFor="telefonoRegistro" required>Teléfono</label>
                     <input
                         type="tel"
                         id="telefonoRegistro"
                         value={telefono}
-                        onChange={(e) => setTelefono(e.target.value)
-                        }
+                        onChange={(e) => setTelefono(e.target.value)}
+                        onFocus={auth.setMostrarError(false)}
                     />
                 </div>
                 <div className="form-group-registro">
-                    <label htmlFor="cuit" required> CUIT:</label>
+                    <label htmlFor="cuit" required> CUIT</label>
                     <input
                         id="cuit"
                         value={cuit}
                         onChange={(e) => setCuit(e.target.value)}
+                        onFocus={auth.setMostrarError(false)}
                     />
                 </div>
                 <div className="form-group-registro">
-                    <label htmlFor="contrasenia" required>Contraseña:</label>
+                    <label htmlFor="contrasenia" required>Contraseña</label>
                     <input
                         type="password"
                         id="contrasenia"
                         value={contrasenia}
                         onChange={(e) => setContrasenia(e.target.value)}
+                        onFocus={auth.setMostrarError(false)}
                     />
                 </div>
                 <div className="form-group-registro">
-                    <label htmlFor="confirmContrasenia" required>Repetir Contraseña:</label>
+                    <label htmlFor="confirmContrasenia" required>Repetir Contraseña</label>
                     <input
                         type="password"
                         id="confirmContrasenia"
                         value={confirmContrasenia}
                         onChange={(e) => setConfirmContrasenia(e.target.value)}
+                        onFocus={auth.setMostrarError(false)}
                     />
                 </div>
                 <div className="botonRegistroContainer">
