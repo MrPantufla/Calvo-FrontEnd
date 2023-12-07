2ee249fc89fa63699a4a2306bf23a81e74ffc319
MrPantufla <juanmapizzioo@gmail.com>
2023-12-06 21:31:50 -0300

    Providers acomodados

--- src/App.js
+++ src/App.js
@@ -7,27 +7,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { LoginProvider } from './contextLogin';
 import { DesplegableProviderCatalogos } from './contextDesplegableCatalogos.jsx';
 import { DesplegableProviderConfiguracion } from './contextDesplegableConfiguracion.jsx';
-import { EditarDatosProvider } from './contextEditarDatos.jsx';
-import { EditarContraseñaContext, EditarContraseñaProvider } from './contextEditarContraseña.jsx';
 
 function App() {
   const apiUrl = "http://localhost:8080/api";
   return (
     <DesplegableProviderConfiguracion>
       <DesplegableProviderCatalogos>
-        <EditarDatosProvider>
-          <EditarContraseñaProvider>
-            <LoginProvider>
-              <Router>
-                <Routes>
-                  <Route path="/home" element={<HomePage />} />
-                  <Route path="/tienda" element={<Ventas />} />
-                  <Route path="/perfil" element={<Perfil />} />
-                </Routes>
-              </Router>
-            </LoginProvider>
-          </EditarContraseñaProvider>
-        </EditarDatosProvider>
+        <LoginProvider>
+          <Router>
+            <Routes>
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/tienda" element={<Ventas />} />
+              <Route path="/perfil" element={<Perfil />} />
+            </Routes>
+          </Router>
+        </LoginProvider>
       </DesplegableProviderCatalogos>
     </DesplegableProviderConfiguracion>
   );
--- src/Perfil/perfil.jsx
+++ src/Perfil/perfil.jsx
@@ -2,14 +2,22 @@ import Header from '../Principal/Header/header';
 import BodyPerfil from './Body Perfil/bodyPerfil';
 import Footer from '../Principal/Footer/footer.jsx';
 import LoginYRegistro from '../Login y registro/loginYRegistro.jsx';
+import { EditarContraseñaProvider } from '../contextEditarContraseña.jsx';
+import { EditarDatosProvider } from '../contextEditarDatos.jsx';
 
-export default function Perfil(){
-    return(
+export default function Perfil() {
+    return (
         <div className="contenedorPrincipalPerfil">
-            <Header/>
-            <LoginYRegistro/>
-            <BodyPerfil/>
-            <Footer/>
+            <Header />
+            <LoginYRegistro />
+
+            <EditarDatosProvider>
+                <EditarContraseñaProvider>
+                    <BodyPerfil />
+                </EditarContraseñaProvider>
+            </EditarDatosProvider>
+
+            <Footer />
         </div>
     );
 }
