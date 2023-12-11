309fcc5c93ec5a6f1bd86044dc6efa0db4ed2c60
MrPantufla <juanmapizzioo@gmail.com>
2023-12-11 11:30:24 -0300

    Colores productoGrande

--- src/Ventas/FiltrosYProductos/productoGrande.jsx
+++ src/Ventas/FiltrosYProductos/productoGrande.jsx
@@ -1,5 +1,9 @@
 import './productoGrande.css';
-import perfil from '../../Imagenes/perfil.jpg';
+import perfil1 from '../../Imagenes/perfil1.png';
+import perfil2 from '../../Imagenes/perfil2.png';
+import perfil3 from '../../Imagenes/perfil3.png';
+import perfil4 from '../../Imagenes/perfil4.png';
+import perfil5 from '../../Imagenes/perfil5.png';
 import logoProductoGrande from '../../Imagenes/logoProductoGrande.png';
 import { useCarrito } from '../../contextCarrito.jsx';
 import { useAuth } from '../../contextLogin.jsx';
@@ -51,13 +55,31 @@ export default function ProductoGrande(args) {
                             <h2>{args.detalle}</h2>
                         </div>
                         <div className="imagenProductoGrandeContainer">
-                            <img className="imagenProductoGrande" src={perfil} />
+                            <img className="imagenProductoGrande" src={args.id % 5 == 0 ?
+                                (perfil1)
+                                :
+                                (args.id % 5 == 1 ?
+                                    (perfil2)
+                                    :
+                                    (args.id % 5 == 2 ?
+                                        (perfil3)
+                                        :
+                                        (args.id % 5 == 3 ?
+                                            (perfil4)
+                                            :
+                                            (perfil5))))} />
                         </div>
                         <p className="textoCantidad">CANTIDAD EN EL CARRITO</p>
                         <div className="cantidad">
                             <button className="botonProductoGrande" onClick={restarContador}>-</button>
                             <p className="cantidadProductoGrande">{cantidad}</p>
                             <button className="botonProductoGrande" onClick={sumarContador}>+</button>
+                            <div className="coloresProductoGrande">
+                                <div className="color natural" />
+                                <div className="color blanco" />
+                                <div className="color negro" />
+                                <div className="color madera" />
+                            </div>
                         </div>
                     </div>
                 </div>
