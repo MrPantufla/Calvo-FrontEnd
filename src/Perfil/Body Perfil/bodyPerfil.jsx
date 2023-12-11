233d22050990224e66f55d3c5e14f86bc14f3504
MrPantufla <juanmapizzioo@gmail.com>
2023-12-11 17:49:20 -0300

    Direcciones agregado

--- src/Perfil/Body Perfil/bodyPerfil.jsx
+++ src/Perfil/Body Perfil/bodyPerfil.jsx
@@ -5,17 +5,21 @@ import EditarDatos from './Configuracion/Editar Datos/editarDatos.jsx';
 import { useEditarDatos } from '../../contextEditarDatos.jsx';
 import EditarContraseña from './Configuracion/Editar Contraseña/editarContraseña.jsx';
 import { useEditarContraseña } from '../../contextEditarContraseña.jsx';
+import { useDirecciones } from '../../contextDireciones.jsx';
+import Direcciones from './Configuracion/Direcciones/direcciones.jsx';
 
 export default function BodyPerfil(){
     const auth = useAuth();
     const editarDatos = useEditarDatos();
     const editarContraseña = useEditarContraseña();
+    const direcciones = useDirecciones();
 
     return(
         <div className="contenedorPrincipalBodyPerfil">
             <Configuracion/>
-            {editarDatos.editarDatosAbierto ? (<EditarDatos/>) : (<div/>) }
-            {editarContraseña.editarContraseñaAbierto ? (<EditarContraseña/>) : (<div/>)}
+            {editarDatos.editarDatosAbierto ? (<EditarDatos/>) : (<></>) }
+            {direcciones.direccionesAbierto ? (<Direcciones/>) : (<></>)}
+            {editarContraseña.editarContraseñaAbierto ? (<EditarContraseña/>) : (<></>)}
             <h1>Hola, {auth.state.userInfo.nombre}</h1>
         </div>
     );
