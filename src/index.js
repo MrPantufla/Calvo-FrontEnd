158c3cff0c71f2d45cf78483a160e35261084eb2
MrPantufla <juanmapizzioo@gmail.com>
2023-12-12 15:55:35 -0300

    FavoritosProvider en app

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { CarritoProvider } from './contextCarrito.jsx';
+import { FavoritosProvider } from './favoritosContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <CarritoProvider>
-    <App/>
+    <FavoritosProvider>
+      <App />
+    </FavoritosProvider>
   </CarritoProvider>
 );
