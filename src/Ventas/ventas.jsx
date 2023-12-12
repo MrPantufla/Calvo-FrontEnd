13130d9369d384d7603eeafb2db62474bcb163bf
MrPantufla <juanmapizzioo@gmail.com>
2023-12-12 11:52:15 -0300

    FavoritosContext agregado

--- src/Ventas/ventas.jsx
+++ src/Ventas/ventas.jsx
@@ -7,39 +7,42 @@ import Footer from '../Principal/Footer/footer.jsx';
 import LoginYRegistro from '../Login y registro/loginYRegistro.jsx';
 import Catalogos from './Catalogos/catalogos.jsx';
 import CartelLogout from '../Logout/cartelLogout.jsx';
+import { FavoritosContext, FavoritosProvider } from '../favoritosContext.jsx';
 import { productos } from '../productos.js'; //Por si la base de datos no anda
 
-export default function Ventas(){
-    const [jsonProductos, setJsonProductos] = useState([]);
+export default function Ventas() {
+  const [jsonProductos, setJsonProductos] = useState([]);
 
-    useEffect(() => {
-        obtenerProductosFiltrados();
-    }, []);
+  useEffect(() => {
+    obtenerProductosFiltrados();
+  }, []);
 
-    const obtenerProductosFiltrados = async () => {
-        try {
-          const response = await fetch(`http://localhost:8080/api/productos`);
-          if (response.ok) {
-            const productosObtenidos = await response.json();
-            setJsonProductos(productosObtenidos);
-          } else {
-            console.error('Error al obtener productos filtrados:', response.statusText);
-          }
-        } catch (error) {
-          console.error('Error en la solicitud:', error);
-        }
+  const obtenerProductosFiltrados = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/productos`);
+      if (response.ok) {
+        const productosObtenidos = await response.json();
+        setJsonProductos(productosObtenidos);
+      } else {
+        console.error('Error al obtener productos filtrados:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error en la solicitud:', error);
     }
+  }
 
-    return(
-        <>
-            <Header/>
-            <Catalogos/>
-            <LoginYRegistro/>
-            <CartelLogout/>
-            <Carrito json={jsonProductos}/>
-            <Favoritos/>
-            <Filtros json={jsonProductos}/>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <FavoritosProvider>
+        <Header />
+        <Catalogos />
+        <LoginYRegistro />
+        <CartelLogout />
+        <Carrito json={jsonProductos} />
+        <Favoritos />
+        <Filtros json={jsonProductos} />
+        <Footer />
+      </FavoritosProvider>
+    </>
+  );
 }
