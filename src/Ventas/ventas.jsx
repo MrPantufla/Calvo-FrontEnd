c15cd9171d2e378ce421902e5442447d12d3ead6
MrPantufla <juanmapizzioo@gmail.com>
2023-12-12 15:55:53 -0300

    .

--- src/Ventas/ventas.jsx
+++ src/Ventas/ventas.jsx
@@ -33,7 +33,7 @@ export default function Ventas() {
 
   return (
     <>
-      <FavoritosProvider>
+      
         <Header />
         <Catalogos />
         <LoginYRegistro />
@@ -42,7 +42,6 @@ export default function Ventas() {
         <Favoritos />
         <Filtros json={jsonProductos} />
         <Footer />
-      </FavoritosProvider>
     </>
   );
 }
