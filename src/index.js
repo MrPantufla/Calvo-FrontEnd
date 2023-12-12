ded1a430471b97844da36bf41ade7ab819f95146
MrPantufla <juanmapizzioo@gmail.com>
2023-12-12 16:30:35 -0300

    app envuelta en loginProvider

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import './index.css';
 import App from './App';
 import { CarritoProvider } from './contextCarrito.jsx';
 import { FavoritosProvider } from './favoritosContext.jsx';
+import { LoginProvider } from './contextLogin.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <LoginProvider>
   <CarritoProvider>
     <FavoritosProvider>
       <App />
     </FavoritosProvider>
   </CarritoProvider>
+  </LoginProvider>
 );
