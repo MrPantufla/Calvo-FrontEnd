ffd2d94854d4072eae0c6b7b278fae5c9e586e7d
MrPantufla <juanmapizzioo@gmail.com>
2023-12-13 17:31:42 -0300

    productosProvider agregado

--- src/index.js
+++ src/index.js
@@ -7,14 +7,17 @@ import App from './App';
 import { CarritoProvider } from './contextCarrito.jsx';
 import { FavoritosProvider } from './favoritosContext.jsx';
 import { LoginProvider } from './contextLogin.jsx';
+import { ProductosProvider } from './contextProductos.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <LoginProvider>
-    <CarritoProvider>
-      <FavoritosProvider>
-        <App />
-      </FavoritosProvider>
-    </CarritoProvider>
-  </LoginProvider>
+  <ProductosProvider>
+    <LoginProvider>
+      <CarritoProvider>
+        <FavoritosProvider>
+          <App />
+        </FavoritosProvider>
+      </CarritoProvider>
+    </LoginProvider>
+  </ProductosProvider>
 );
