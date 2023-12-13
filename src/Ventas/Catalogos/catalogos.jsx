08ffd7cc4a9cb335f915e7cd436a27774d44c1b5
MrPantufla <juanmapizzioo@gmail.com>
2023-12-13 17:28:52 -0300

    .

--- src/Ventas/Catalogos/catalogos.jsx
+++ src/Ventas/Catalogos/catalogos.jsx
@@ -20,22 +20,22 @@ export default function Catalogos() {
             const maxCatalogosTop = 8.7; // ajusta el valor máximo de altura según tus necesidades
             const minCatalogosTop = 6.2; // ajusta el valor mínimo de altura según tus necesidades
             const alturaHeader = 150; // ajusta según tus necesidades
-    
+
             // Calcula la nueva posición top en función del scroll
             let newTop =
                 maxCatalogosTop -
                 (maxCatalogosTop - minCatalogosTop) * (scrollPosition / alturaHeader);
-    
+
             // Asegúrate de que newTop no sea menor que el valor mínimo de 7
             newTop = Math.max(minCatalogosTop, newTop);
-    
+
             // Establece la nueva posición top
             setCatalogosTop(newTop);
         };
-    
+
         // Agrega el event listener para el scroll
         window.addEventListener('scroll', handleScroll);
-    
+
         // Limpia el event listener al desmontar el componente
         return () => {
             window.removeEventListener('scroll', handleScroll);
