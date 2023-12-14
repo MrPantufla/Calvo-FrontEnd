402c2c9d99b4d3bc00caf99cdfc030b44cf3e438
MrPantufla <juanmapizzioo@gmail.com>
2023-12-14 17:46:24 -0300

    .

--- src/Perfil/Body Perfil/Configuracion/Solicitar Contraseña/solicitarContraseña.jsx
+++
@@ -1,9 +0,0 @@
-import 'solicitarContraseña.css';
-
-export default function SolicitarContraseña(){
-    return(
-        <div className="contenedorPricipalSolicitarContraseña">
-            
-        </div>
-    );
-}
