ddbb9147098f5c5936f3b1f96727bc809f01eb68
MrPantufla <juanmapizzioo@gmail.com>
2023-12-14 17:46:59 -0300

    misCompras inicializado

---
+++ src/Mis compras/misCompras.jsx
@@ -0,0 +1,14 @@
+import './misCompras.css';
+
+export default function MisCompras(){
+
+    
+
+    return(
+        <div className="contenedorPrincipalMisCompras">
+            <div className="misComprasContainer">
+                
+            </div>
+        </div>
+    );
+}
