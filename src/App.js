e63db50cb510b4bc4790cd95f50928bf3301b72a
MrPantufla <juanmapizzioo@gmail.com>
2023-12-14 17:51:46 -0300

    .

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import Ventas from './Ventas/ventas.jsx';
 import Perfil from './Perfil/perfil.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { DesplegableProviderCatalogos } from './contextDesplegableCatalogos.jsx';
-import { DesplegableProviderConfiguracion } from './contextDesplegableConfiguracion.jsx';
 
 function App() {
   const apiUrl = "http://localhost:8080/api";
   return (
 
-      <DesplegableProviderConfiguracion>
         <DesplegableProviderCatalogos>
           <Router>
             <Routes>
@@ -21,7 +19,6 @@ function App() {
             </Routes>
           </Router>
         </DesplegableProviderCatalogos>
-      </DesplegableProviderConfiguracion>
   );
 }
 
