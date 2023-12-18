82835ec628c7f10cd206a006c9086914a78cf92a
MrPantufla <juanmapizzioo@gmail.com>
2023-12-18 17:49:02 -0300

    .

--- src/Login y registro/Login/login.jsx
+++ src/Login y registro/Login/login.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './login.css';
 import { useAuth } from '../../contextLogin.jsx';
-import logoLoginYRegistro from '../../Imagenes/logoLoginYRegistro.jpeg';
 
 export default function Login() {
   const [email, setEmail] = useState('');
