3b7a7adfc020bd2b7e150bd47466cb6f9dd6af7e
MrPantufla <juanmapizzioo@gmail.com>
2023-12-19 17:16:42 -0300

    ConfiguracionProvider movido

--- src/Perfil/perfil.jsx
+++ src/Perfil/perfil.jsx
@@ -2,17 +2,14 @@ import Header from '../Principal/Header/header';
 import BodyPerfil from './Body Perfil/bodyPerfil';
 import Footer from '../Principal/Footer/footer.jsx';
 import LoginYRegistro from '../Login y registro/loginYRegistro.jsx';
-import { ConfiguracionProvider } from '../contextConfiguracion.jsx';
 
 export default function Perfil() {
     return (
         <div className="contenedorPrincipalPerfil">
-            <ConfiguracionProvider>
                 <Header />
                 <LoginYRegistro />
                 <BodyPerfil />
                 <Footer />
-            </ConfiguracionProvider>
         </div>
     );
 }
