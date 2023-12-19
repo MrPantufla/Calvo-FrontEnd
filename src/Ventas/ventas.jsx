40f40f9d6d3a204a802615bf6ef9daa0fbb069c6
MrPantufla <juanmapizzioo@gmail.com>
2023-12-19 17:19:44 -0300

    confirmaciones agregadas

--- src/Ventas/ventas.jsx
+++ src/Ventas/ventas.jsx
@@ -7,20 +7,23 @@ import Footer from '../Principal/Footer/footer.jsx';
 import LoginYRegistro from '../Login y registro/loginYRegistro.jsx';
 import Catalogos from './Catalogos/catalogos.jsx';
 import CartelLogout from '../Logout/cartelLogout.jsx';
+import { useCarrito } from '../contextCarrito.jsx';
+import ConfirmarCompra from './Confirmar compra/confirmarCompra.jsx';
 import { productos } from '../productos.js'; //Por si la base de datos no anda
 
 export default function Ventas() {
-
+  const carrito = useCarrito();
   return (
     <>
-        <Header />
-        <Catalogos />
-        <LoginYRegistro />
-        <CartelLogout />
-        <Carrito/>
-        <Favoritos />
-        <Filtros/>
-        <Footer />
+      <Header />
+      <Catalogos />
+      <LoginYRegistro />
+      <CartelLogout />
+      <Carrito />
+      <Favoritos />
+      <Filtros />
+      {carrito.confirmarCompraAbierto ? (<ConfirmarCompra/>) : (<></>)}
+      <Footer />
     </>
   );
 }
