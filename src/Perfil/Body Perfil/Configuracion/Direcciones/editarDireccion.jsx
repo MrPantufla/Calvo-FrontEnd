f7b1aa135f973f36b19cd80b1d1b95d054849e0a
MrPantufla <juanmapizzioo@gmail.com>
2023-12-19 17:15:59 -0300

    Movidos formulaio y fetch

--- src/Perfil/Body Perfil/Configuracion/Direcciones/editarDireccion.jsx
+++ src/Perfil/Body Perfil/Configuracion/Direcciones/editarDireccion.jsx
@@ -3,52 +3,10 @@ import { useEffect, useState } from 'react';
 import { useAuth } from '../../../../contextLogin';
 import { useConfiguracion } from '../../../../contextConfiguracion';
 import { useDireccion } from '../../../../contextDireccion';
+import FormularioDireccion from './formularioDireccion';
 
 export default function Direcciones() {
-    const auth = useAuth();
     const configuracion = useConfiguracion();
-    const direccion = useDireccion();
-
-    const handleEnviarDireccion = () => {
-        fetch('http://localhost:8080/api/direcciones', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': auth.state.userInfo.token
-            },
-            body: JSON.stringify(direccionEstructura),
-            credentials: 'include',
-        })
-            .then(response => {
-                if (response.ok) {
-                    console.log('Envío de dirección exitoso');
-                    return null;
-                } else {
-                    return response.text();
-                }
-            })
-            .then(data => {
-                if (data !== null) {
-                    console.log('Respuesta (texto): ', data);
-                    auth.setErrorMessage(data);
-                }
-            })
-            .catch(error => {
-                console.error('Ocurrió un error al enviar los datos:', error.message);
-                if (error.message.includes('409')) {
-                    console.error('Conflicto al intentar enviar la dirección');
-                    auth.setErrorMessage('Ocurrió un error');
-                }
-            });
-    };
-
-    const direccionEstructura = {
-        calle: direccion.calle,
-        numero: direccion.numero,
-        cp: direccion.cp,
-        localidad: direccion.localidad,
-        provincia: direccion.provincia
-    };
 
     const toggleCollapse = () =>{
         configuracion.direccionAbierto ? (configuracion.cerrarDireccion()) : (configuracion.abrirDireccion())
@@ -69,66 +27,7 @@ export default function Direcciones() {
                 </div>
             </div>
             <div className={`colapsableEditarDireccion ${configuracion.direccionAbierto ? 'open' : ''}`}>
-                <h2>DIRECCIÓN DE ENVÍO</h2>
-                <div className="formularioAgregarDireccion">
-                    <form>
-                        <div className="form-group-direcciones">
-                            <label htmlFor="calle" required>Calle</label>
-                            <input
-                                required
-                                type="text"
-                                id="calle"
-                                value={direccion.calle}
-                                onChange={(e) => direccion.setCalle(e.target.value)}
-                            />
-                        </div>
-                        <div className="form-group-direcciones">
-                            <label htmlFor="numero" required>Numero</label>
-                            <input
-                                required
-                                type="text"
-                                id="numero"
-                                value={direccion.numero}
-                                onChange={(e) => direccion.setNumero(e.target.value)}
-                            />
-                        </div>
-                        <div className="form-group-direcciones">
-                            <label htmlFor="cp" required>Código postal</label>
-                            <input
-                                required
-                                type="text"
-                                id="cp"
-                                value={direccion.cp}
-                                onChange={(e) => direccion.setCp(e.target.value)}
-                            />
-                        </div>
-                        <div className="form-group-direcciones">
-                            <label htmlFor="localidad" required>Localidad</label>
-                            <input
-                                required
-                                type="text"
-                                id="localidad"
-                                value={direccion.localidad}
-                                onChange={(e) => direccion.setLocalidad(e.target.value)}
-                            />
-                        </div>
-                        <div className="form-group-direcciones">
-                            <label htmlFor="provincia" required>Provincia</label>
-                            <input
-                                required
-                                type="text"
-                                id="provincia"
-                                value={direccion.provincia}
-                                onChange={(e) => direccion.setProvincia(e.target.value)}
-                            />
-                        </div>
-                        <div className="botonEnviarDireccionContainer">
-                            <button className="botonEnviarDireccion" type="button" onClick={handleEnviarDireccion}>
-                                Aceptar
-                            </button>
-                        </div>
-                    </form>
-                </div>
+                <FormularioDireccion/>
             </div>
         </div>
     );
