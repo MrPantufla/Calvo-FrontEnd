4b3f26aa91cb3099d184e1482a9a180d7f8b6e3e
MrPantufla <juanmapizzioo@gmail.com>
2023-12-20 11:55:02 -0300

    Estilo acorde al nuevo header

--- src/Ventas/Catalogos/catalogos.jsx
+++ src/Ventas/Catalogos/catalogos.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 
 export default function Catalogos() {
     const desplegable = useDesplegableCatalogos();
-    const [catalogosTop, setCatalogosTop] = useState(8.7);
+    const [catalogosTop, setCatalogosTop] = useState(8.1);
 
     const handleDownload = (filename) => {
         const filePath = `${process.env.PUBLIC_URL}/Archivos/${filename}.pdf`;
@@ -17,8 +17,8 @@ export default function Catalogos() {
     useEffect(() => {
         const handleScroll = () => {
             const scrollPosition = window.scrollY;
-            const maxCatalogosTop = 8.7; // ajusta el valor máximo de altura según tus necesidades
-            const minCatalogosTop = 6.2; // ajusta el valor mínimo de altura según tus necesidades
+            const maxCatalogosTop = 8.1; // ajusta el valor máximo de altura según tus necesidades
+            const minCatalogosTop = 5.6; // ajusta el valor mínimo de altura según tus necesidades
             const alturaHeader = 150; // ajusta según tus necesidades
 
             // Calcula la nueva posición top en función del scroll
