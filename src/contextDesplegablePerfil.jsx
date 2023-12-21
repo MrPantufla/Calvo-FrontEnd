cc18c7fb2491f327e92d650d29938b7a7a2f3331
MrPantufla <juanmapizzioo@gmail.com>
2023-12-21 17:52:30 -0300

    contextDesplegablePerfil creado

---
+++ src/contextDesplegablePerfil.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+const DesplegablePerfilContext = createContext();
+
+function useDesplegablePerfil() {
+    return useContext(DesplegablePerfilContext);
+}
+
+function DesplegableProviderPerfil({ children }) {
+    const [perfilHovered, setPerfilHovered] = useState(false);
+
+    const abrirPerfil = () => {
+        setPerfilHovered(true);
+    }
+
+    const cerrarPerfil = () => {
+        setPerfilHovered(false);
+    }
+
+    return (
+        <DesplegablePerfilContext.Provider value={{ perfilHovered, setPerfilHovered, abrirPerfil, cerrarPerfil }}>
+            {children}
+        </DesplegablePerfilContext.Provider>
+    );
+}
+
+export { DesplegablePerfilContext, useDesplegablePerfil, DesplegableProviderPerfil };
