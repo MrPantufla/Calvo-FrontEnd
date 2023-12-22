57c69e45b07ea08cbe2b859dfc758fff6e716e6b
MrPantufla <juanmapizzioo@gmail.com>
2023-12-22 10:48:04 -0300

    DesplegablePerfil importado

--- src/Perfil/perfil.jsx
+++ src/Perfil/perfil.jsx
@@ -2,11 +2,13 @@ import Header from '../Principal/Header/header';
 import BodyPerfil from './Body Perfil/bodyPerfil';
 import Footer from '../Principal/Footer/footer.jsx';
 import LoginYRegistro from '../Login y registro/loginYRegistro.jsx';
+import DesplegablePerfil from '../Principal/Header/Desplegable perfil/desplegablePerfil.jsx';
 
 export default function Perfil() {
     return (
         <div className="contenedorPrincipalPerfil">
                 <Header />
+                <DesplegablePerfil/>
                 <LoginYRegistro />
                 <BodyPerfil />
                 <Footer />
