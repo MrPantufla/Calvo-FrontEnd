ecdb0bb9ec11ce8b43c4b15be3ad1bc2776d95ae
MrPantufla <juanmapizzioo@gmail.com>
2023-12-27 16:20:10 -0300

    cambiada logica de actualizacion carrito

--- src/contextCarrito.jsx
+++ src/contextCarrito.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { useProductos } from './contextProductos';
 import { useAuth } from './contextLogin';
 
@@ -10,13 +10,44 @@ function useCarrito() {
 
 function CarritoProvider({ children }) {
   const productos = useProductos();
+  const auth = useAuth();
+
   const [elementos, setElementos] = useState([]);
   const [carritoAbierto, setCarritoAbierto] = useState(false);
-  const auth = useAuth();
-  const [primeraAccion, setPrimeraAccion] = useState(true);
-  const [band, setBand] = useState(true);
   const [carritoConfirmado, setCarritoConfirmado] = useState(false);
   const [confirmarCompraAbierto, setConfirmarCompraAbierto] = useState(false);
+
+  useEffect(() => {
+    // Esta función se ejecutará cada vez que elementos cambie
+    const listaCarrito = [...elementos];
+    const productos = listaCarrito.map(item => item.id).join(' ');
+    const cantidades = listaCarrito.map(item => item.cantidad).join(' ');
+    const ActualizacionCarrito = {
+      productos: productos,
+      cantidades: cantidades
+    }
+    fetch('http://localhost:8080/api/actualizarCarrito', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(ActualizacionCarrito),
+    })
+      .then(response => {
+        if (response.ok) {
+          // Procesar la respuesta exitosa
+          console.log('Envío de carrito exitoso');
+        } else {
+          // Procesar la respuesta de error
+          console.error('Error en el envío de carrito:', response.statusText);
+        }
+      })
+      .catch(error => {
+        console.error('Error al enviar la lista del carrito al servidor:', error);
+      });
+
+  }, [elementos]);
 
   function añadirElemento(id, cantidad) {
     if (Object.keys(productos.productosIndexado).length !== 0) {
@@ -30,36 +61,14 @@ function CarritoProvider({ children }) {
 
           if (elementoExistente) {
             elementoExistente.cantidad += cantidad;
+            // Si ya existe, solo actualiza la cantidad y deja que el useEffect maneje la actualización del carrito
             return [...prevElementos];
           } else {
-            const nuevoElemento = { id, cod_origProducto, cantidad, detalleProducto, precioProducto };
-            return [...prevElementos, nuevoElemento];
+            // Si es un nuevo elemento, agrega y deja que el useEffect maneje la actualización del carrito
+            return [...prevElementos, { id, cod_origProducto, cantidad, detalleProducto, precioProducto }];
           }
         }
       });
-    }
-  }
-
-  useEffect(() => {
-    if (!primeraAccion) {
-      actualizarCarrito();
-    }
-    else {
-      setPrimeraAccion(false);
-    }
-  }, [elementos]);
-
-  function toggleCarrito() {
-    if (auth.state.logueado) {
-      if (auth.state.userInfo.email_confirmado) {
-        setCarritoAbierto(!carritoAbierto);
-      }
-      else {
-        auth.setMostrarLogin(true);
-      }
-    }
-    else {
-      auth.setMostrarLogin(true);
     }
   }
 
@@ -70,19 +79,28 @@ function CarritoProvider({ children }) {
       elementoExistente.cantidad -= 1;
       setElementos([...elementos]);
     } else {
-      setElementos((prevElementos) => prevElementos.filter((elemento) => elemento.id !== id));
+      eliminarElemento(id);
     }
   }
 
   function eliminarElemento(id) {
-    const elementoExistente = elementos.find((elemento) => elemento.id === id);
-    while (elementoExistente.cantidad !== 1) {
-      restarElemento(id);
+    setElementos((prevElementos) => prevElementos.filter((elemento) => elemento.id !== id));
+  }
+
+  function toggleCarrito() {
+    if (auth.state.logueado) {
+      if (auth.state.userInfo.email_confirmado) {
+        setCarritoAbierto(!carritoAbierto);
+      } else {
+        auth.setMostrarLogin(true);
+      }
+    } else {
+      auth.setMostrarLogin(true);
     }
-    restarElemento(id);
   }
 
   function actualizarCantidadElemento(id, nuevaCantidad) {
+    console.log("ENTRA EN ACTUALIZARCANTIDADELEMENTO")
     const elementoExistente = elementos.find((elemento) => elemento.id === id);
     if (elementoExistente) {
       elementoExistente.cantidad = nuevaCantidad;
@@ -95,36 +113,36 @@ function CarritoProvider({ children }) {
   }
 
   const actualizarCarrito = () => {
-    if (band == true) {
-      const listaCarrito = [...elementos];
-      const productos = listaCarrito.map(item => item.id).join(' ');
-      const cantidades = listaCarrito.map(item => item.cantidad).join(' ');
-      const ActualizacionCarrito = {
-        productos: productos,
-        cantidades: cantidades
-      }
+    console.log("ENTRA EN ACTUALIZARCARRITO");
 
-      fetch('http://localhost:8080/api/actualizarCarrito', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': auth.tokenCookie
-        },
-        body: JSON.stringify(ActualizacionCarrito),
-      })
-        .then(response => {
-          if (response.ok) {
-            // Procesar la respuesta exitosa
-            console.log('Envío de carrito exitoso');
-          } else {
-            // Procesar la respuesta de error
-            console.error('Error en el envío de carrito:', response.statusText);
-          }
-        })
-        .catch(error => {
-          console.error('Error al enviar la lista del carrito al servidor:', error);
-        });
+    const listaCarrito = [...elementos];
+    const productos = listaCarrito.map(item => item.id).join(' ');
+    const cantidades = listaCarrito.map(item => item.cantidad).join(' ');
+    const ActualizacionCarrito = {
+      productos: productos,
+      cantidades: cantidades
     }
+
+    fetch('http://localhost:8080/api/actualizarCarrito', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify(ActualizacionCarrito),
+    })
+      .then(response => {
+        if (response.ok) {
+          // Procesar la respuesta exitosa
+          console.log('Envío de carrito exitoso');
+        } else {
+          // Procesar la respuesta de error
+          console.error('Error en el envío de carrito:', response.statusText);
+        }
+      })
+      .catch(error => {
+        console.error('Error al enviar la lista del carrito al servidor:', error);
+      });
   }
 
   const confirmarCompra = () => {
@@ -133,7 +151,6 @@ function CarritoProvider({ children }) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': auth.tokenCookie,
       },
       body: JSON.stringify(nuevosElementos),
       credentials: 'include',
@@ -148,8 +165,6 @@ function CarritoProvider({ children }) {
   }
 
   useEffect(() => {
-    setBand(false);
-
     if (auth.state.logueado && auth.state.userInfo.cantidades_carrito) {
       const productosArray = auth.state.userInfo.productos_carrito.split(' ').map(Number);
       const cantidadesArray = auth.state.userInfo.cantidades_carrito.split(' ').map(Number);
@@ -158,11 +173,10 @@ function CarritoProvider({ children }) {
         añadirElemento(productosArray[i], cantidadesArray[i])
       }
     }
-    setBand(true);
-  }, [auth.state.logueado, productos.productosIndexado]);
+  }, [productos.productosIndexado]);
 
   return (
-    <CarritoContext.Provider value={{confirmarCompraAbierto, setConfirmarCompraAbierto ,carritoConfirmado, setCarritoConfirmado, confirmarCompra, toggleCarrito, setCarritoAbierto, carritoAbierto, elementos, limpiarCarrito, añadirElemento, restarElemento, actualizarCantidadElemento, eliminarElemento }}>
+    <CarritoContext.Provider value={{ actualizarCarrito, confirmarCompraAbierto, setConfirmarCompraAbierto, carritoConfirmado, setCarritoConfirmado, confirmarCompra, toggleCarrito, setCarritoAbierto, carritoAbierto, elementos, limpiarCarrito, añadirElemento, restarElemento, actualizarCantidadElemento, eliminarElemento }}>
       {children}
     </CarritoContext.Provider>
   );
