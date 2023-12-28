c9e755cfadc15a5a7bba5ade47c235f25e7f96d0
MrPantufla <juanmapizzioo@gmail.com>
2023-12-28 11:23:03 -0300

    .

--- src/Perfil/perfil.jsx
+++ src/Perfil/perfil.jsx
@@ -11,7 +11,7 @@ export default function Perfil() {
                 <DesplegablePerfil/>
                 <LoginYRegistro />
                 <BodyPerfil />
-                <Footer />
+                {/*<Footer />*/}
         </div>
     );
 }
--- src/Principal/Body/Informacion y contacto/contacto.jsx
+++ src/Principal/Body/Informacion y contacto/contacto.jsx
@@ -25,6 +25,7 @@ export default function Contacto() {
             setFormError(false);
             event.target.submit();
         }
+
     }
 
     return (
