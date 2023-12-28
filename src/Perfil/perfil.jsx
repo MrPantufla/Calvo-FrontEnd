46df4e726ac29eacf55bf9cf188069ac3018f960
MrPantufla <juanmapizzioo@gmail.com>
2023-12-28 17:02:54 -0300

    Navigate a home al entrar sin loguear

--- src/Perfil/perfil.jsx
+++ src/Perfil/perfil.jsx
@@ -3,15 +3,28 @@ import BodyPerfil from './Body Perfil/bodyPerfil';
 import Footer from '../Principal/Footer/footer.jsx';
 import LoginYRegistro from '../Login y registro/loginYRegistro.jsx';
 import DesplegablePerfil from '../Principal/Header/Desplegable perfil/desplegablePerfil.jsx';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useAuth } from '../contextLogin.jsx';
 
 export default function Perfil() {
+    const navigate = useNavigate();
+    const auth = useAuth();
+
+    useEffect(() => {
+        console.log("ENTRADA")
+        if (!auth.state.logueado) {
+            navigate("/home");
+        }
+    })
+
     return (
         <div className="contenedorPrincipalPerfil">
-                <Header />
-                <DesplegablePerfil/>
-                <LoginYRegistro />
-                <BodyPerfil />
-                {/*<Footer />*/}
+            <Header />
+            <DesplegablePerfil />
+            <LoginYRegistro />
+            <BodyPerfil />
+            {/*<Footer />*/}
         </div>
     );
 }
