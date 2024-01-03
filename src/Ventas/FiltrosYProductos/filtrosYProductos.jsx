1ddfd99e50c03bef3e3b3f611ffe8ab39ef43381
MrPantufla <juanmapizzioo@gmail.com>
2024-01-03 17:50:23 -0300

    Agregada flecha en PERFIL

--- src/Ventas/FiltrosYProductos/filtrosYProductos.jsx
+++ src/Ventas/FiltrosYProductos/filtrosYProductos.jsx
@@ -119,7 +119,9 @@ export default function FiltrosYProductos() {
                   id={tipo_prod + "Id"}
                 />
                 <div className="textoRubro">
-                  {tipo_prod}
+                  {tipo_prod} {tipo_prod == 'PERFIL' ? (<svg xmlns="http://www.w3.org/2000/svg" width="1.7rem" height="1.7rem" fill="currentColor" className="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                    <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+                  </svg>) : (<></>)}
                 </div>
               </div>
               {tipo_prod == 'PERFIL' ?
