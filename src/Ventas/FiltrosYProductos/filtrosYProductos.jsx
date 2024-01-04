e35bb47966dd00c0044f0cdb8c18befdbe218438
MrPantufla <juanmapizzioo@gmail.com>
2024-01-04 10:27:51 -0300

    Cambios de estilo ordenamiento

--- src/Ventas/FiltrosYProductos/filtrosYProductos.jsx
+++ src/Ventas/FiltrosYProductos/filtrosYProductos.jsx
@@ -95,7 +95,7 @@ export default function FiltrosYProductos() {
           >
           </input>
           <div className="lupaContainer">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.6rem" height="1.6rem" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
             </svg>
           </div>
@@ -150,12 +150,10 @@ export default function FiltrosYProductos() {
             </label>
           ))}
         </div>
-        <div className="botonesOrdenamientoContainer">
-          <BotonesOrdenamiento />
-        </div>
       </div>
 
       <div className="productos">
+        <BotonesOrdenamiento/>
         <div className="row">
           {itemsActuales.map((producto) => (
             <div key={producto.id} className="col-12 col-md-4 producto">
