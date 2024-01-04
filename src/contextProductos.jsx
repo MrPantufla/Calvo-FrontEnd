e312f30008de66d7d3c6e72a17820638880d95a5
MrPantufla <juanmapizzioo@gmail.com>
2024-01-04 10:28:14 -0300

    cod_orig agregado a ordenamiento

--- src/contextProductos.jsx
+++ src/contextProductos.jsx
@@ -12,6 +12,8 @@ function ProductosProvider({ children }) {
     const [precioDescActivo, setPrecioDescActivo] = useState(false);
     const [kgAscActivo, setKgAscActivo] = useState(false);
     const [kgDescActivo, setKgDescActivo] = useState(false);
+    const [cod_origAscActivo, setCod_origAscActivo] = useState(false);
+    const [cod_origDescActivo, setCod_origDescActivo] = useState(false);
     const [ordenamientoActivo, setOrdenamientoActivo] = useState('null');
     const [jsonProductos, setJsonProductos] = useState([]);
 
@@ -40,12 +42,22 @@ function ProductosProvider({ children }) {
     }, {});
 
     const ordenarPorPrecioAsc = (productos) => {
-        return productos.sort((prodA, prodB) => prodA.precio - prodB.precio);
+        return productos.sort((prodA, prodB) => {
+            const precioA = prodA.kg !== 0 ? prodA.precio * prodA.kg : prodA.precio;
+            const precioB = prodB.kg !== 0 ? prodB.precio * prodB.kg : prodB.precio;
+
+            return precioA - precioB;
+        });
     }
 
     const ordenarPorPrecioDesc = (productos) => {
-        return productos.sort((prodA, prodB) => prodB.precio - prodA.precio);
-    }
+        return productos.sort((prodA, prodB) => {
+            const precioA = prodA.kg !== 0 ? prodA.precio * prodA.kg : prodA.precio;
+            const precioB = prodB.kg !== 0 ? prodB.precio * prodB.kg : prodB.precio;
+
+            return precioB - precioA;
+        });
+    };
 
     const ordenarPorKgAsc = (productos) => {
         return productos.sort((prodA, prodB) => prodA.kg - prodB.kg);
@@ -53,6 +65,40 @@ function ProductosProvider({ children }) {
 
     const ordenarPorKgDesc = (productos) => {
         const nuevosProductos = productos.sort((prodA, prodB) => prodB.kg - prodA.kg);
+        return nuevosProductos;
+    }
+
+    const ordenarPorCod_origAsc = (productos) => {
+        const nuevosProductos = productos.sort((prodA, prodB) => {
+            const cod_origA = prodA.cod_orig.toLowerCase();
+            const cod_origB = prodB.cod_orig.toLowerCase();
+
+            if (cod_origA < cod_origB) {
+                return -1;
+            }
+            if (cod_origA > cod_origB) {
+                return 1;
+            }
+            return 0;
+        });
+
+        return nuevosProductos;
+    }
+
+    const ordenarPorCod_origDesc = (productos) => {
+        const nuevosProductos = productos.sort((prodA, prodB) => {
+            const cod_origA = prodA.cod_orig.toLowerCase();
+            const cod_origB = prodB.cod_orig.toLowerCase();
+
+            if (cod_origA > cod_origB) {
+                return -1;
+            }
+            if (cod_origA < cod_origB) {
+                return 1;
+            }
+            return 0;
+        });
+
         return nuevosProductos;
     }
 
@@ -66,6 +112,10 @@ function ProductosProvider({ children }) {
                 return ordenarPorKgAsc(productos);
             case 'kgDesc':
                 return ordenarPorKgDesc(productos);
+            case 'cod_origAsc':
+                return ordenarPorCod_origAsc(productos);
+            case 'cod_origDesc':
+                return ordenarPorCod_origDesc(productos);
             default:
                 return productos;
         }
@@ -79,6 +129,10 @@ function ProductosProvider({ children }) {
             kgDescActivo,
             setPrecioAscActivo,
             setPrecioDescActivo,
+            cod_origAscActivo,
+            cod_origDescActivo,
+            setCod_origAscActivo,
+            setCod_origDescActivo,
             setKgAscActivo,
             setKgDescActivo,
             ordenarProductos,
