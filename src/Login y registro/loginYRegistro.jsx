99649a4d78f96a671b0324d85e3dae5937297a6d
MrPantufla <juanmapizzioo@gmail.com>
2024-01-05 16:58:02 -0300

    errorMessage se borra correctamente al abrir el login

--- src/Login y registro/loginYRegistro.jsx
+++ src/Login y registro/loginYRegistro.jsx
@@ -12,7 +12,7 @@ export default function LoginYRegistro() {
 
   useEffect(() => {
     auth.setErrorMessage('');
-  }, []);
+  }, [auth.mostrarLogin]);
 
   const handleOpcionClick = (opcion) => {
     auth.setErrorMessage('');
