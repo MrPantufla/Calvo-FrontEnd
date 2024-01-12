90f7ad7c252c8e9187d5c6b6a7fea3af2044893c
MrPantufla <juanmapizzioo@gmail.com>
2024-01-12 11:50:27 -0300

    Estilos botones

--- src/Perfil/Body Perfil/Configuracion/Editar email/editarEmail.jsx
+++ src/Perfil/Body Perfil/Configuracion/Editar email/editarEmail.jsx
@@ -20,6 +20,8 @@ export default function EditarEmail() {
     const [codigoNuevoEmailEnviado, setCodigoNuevoEmailEnviado] = useState(false);
     const [codigoEmailActualAceptado, setCodigoEmailActualAceptado] = useState();
     const [codigoNuevoEmailAceptado, setCodigoNuevoEmailAceptado] = useState();
+    const [focusEmailActual, setFocusEmailActual] = useState(false);
+    const [focusNuevoEmail, setFocusNuevoEmail] = useState(false);
 
     const vaciarError = () => {
         setErrorMessage('');
@@ -213,7 +215,7 @@ export default function EditarEmail() {
         setErrorMessage('');
     }
 
-    const borrarAdvertence = () =>{
+    const borrarAdvertence = () => {
         setAdvertenceMessage('');
     }
 
@@ -259,37 +261,29 @@ export default function EditarEmail() {
                                         <label htmlFor="codigoEmailActual" id="codigoEmailActual" />
                                         <input
                                             type="text"
-                                            id="codigoEmailActual"
+                                            id="inputCodigoEmail"
+                                            className="inputCodigoEmail"
                                             value={codigoEmailActual}
                                             required
                                             onChange={(e) => setCodigoEmailActual(e.target.value)}
-                                            onFocus={() => { borrarError(); borrarAdvertence(); }}
-                                            placeholder="Ingresa el código"
+                                            onFocus={() => { borrarError(); borrarAdvertence(); setFocusEmailActual(true)}}
+                                            placeholder={focusEmailActual ? '' : 'Ingresa el código'}
                                             disabled={codigoEmailActualAceptado == true}
+                                            onBlur={() => setFocusEmailActual(false)}
                                         />
-                                        <button className="botonVerificarCodigoEmail" onClick={() => verificarCodigo({ codigo: codigoEmailActual, email: auth.state.userInfo.email })} disabled={codigoEmailActualAceptado == true}>
-                                            {codigoEmailActualAceptado == true ?
-                                                (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" className="bi bi-send-check" viewBox="0 0 16 16">
-                                                    <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855a.75.75 0 0 0-.124 1.329l4.995 3.178 1.531 2.406a.5.5 0 0 0 .844-.536L6.637 10.07l7.494-7.494-1.895 4.738a.5.5 0 1 0 .928.372l2.8-7Zm-2.54 1.183L5.93 9.363 1.591 6.602l11.833-4.733Z" />
-                                                    <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686Z" />
-                                                </svg>)
-                                                :
-                                                (codigoEmailActualAceptado == false ?
-                                                    (
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-dash" viewBox="0 0 16 16">
-                                                            <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855a.75.75 0 0 0-.124 1.329l4.995 3.178 1.531 2.406a.5.5 0 0 0 .844-.536L6.637 10.07l7.494-7.494-1.895 4.738a.5.5 0 1 0 .928.372l2.8-7Zm-2.54 1.183L5.93 9.363 1.591 6.602l11.833-4.733Z" />
-                                                            <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-5.5 0a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 0-1h-3a.5.5 0 0 0-.5.5" />
-                                                        </svg>
-                                                    )
-                                                    :
-                                                    (
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
-                                                            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-                                                        </svg>
-                                                    )
-                                                )
-                                            }
-                                        </button>
+                                        {!codigoEmailActualAceptado ?
+                                            (<button className="botonVerificarCodigoEmail" onClick={() =>verificarCodigo({ codigo: codigoEmailActual, email: auth.state.userInfo.email })} disabled={codigoEmailActualAceptado == true}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
+                                                    <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                                                </svg>
+                                            </button>)
+                                            :
+                                            (<button className="botonVerificarCodigoEmail emailAceptado">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-check" viewBox="0 0 16 16">
+                                                    <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855a.75.75 0 0 0-.124 1.329l4.995 3.178 1.531 2.406a.5.5 0 0 0 .844-.536L6.637 10.07l7.494-7.494-1.895 4.738a.5.5 0 1 0 .928.372zm-2.54 1.183L5.93 9.363 1.591 6.602z" />
+                                                    <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686" />
+                                                </svg>
+                                            </button>)}
                                     </div>)
                                     :
                                     <button className="botonEnviarCodigoEditarEmail" onClick={() => { enviarCodigo({ email: auth.state.userInfo.email, tipo: 'actual' }); setAdvertenceMessage(`Código enviado a ${auth.state.userInfo.email}`); }}>Enviar código</button>
@@ -303,37 +297,29 @@ export default function EditarEmail() {
                                         <input
                                             type="text"
                                             id="codigoNuevoEmail"
+                                            className="inputCodigoEmail"
                                             value={codigoNuevoEmail}
                                             required
                                             onChange={(e) => setCodigoNuevoEmail(e.target.value)}
-                                            onFocus={() => { borrarError(); borrarAdvertence(); }}
-                                            placeholder="Ingresa el código"
+                                            onFocus={() => { borrarError(); borrarAdvertence(); setFocusNuevoEmail(true) }}
+                                            placeholder={focusNuevoEmail ? '' : 'Ingresa el código'}
                                             disabled={codigoNuevoEmailAceptado == true}
+                                            onBlur={() => setFocusNuevoEmail(false)}
                                         />
-                                        <button className="botonVerificarCodigoEmail" onClick={() => verificarCodigo({ codigo: codigoNuevoEmail, email: nuevoEmail })} disabled={codigoNuevoEmailAceptado == true}>
-                                            {codigoNuevoEmailAceptado == true ?
-                                                (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" className="bi bi-send-check" viewBox="0 0 16 16">
-                                                    <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855a.75.75 0 0 0-.124 1.329l4.995 3.178 1.531 2.406a.5.5 0 0 0 .844-.536L6.637 10.07l7.494-7.494-1.895 4.738a.5.5 0 1 0 .928.372l2.8-7Zm-2.54 1.183L5.93 9.363 1.591 6.602l11.833-4.733Z" />
-                                                    <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686Z" />
-                                                </svg>)
-                                                :
-                                                (
-                                                    codigoNuevoEmailAceptado == false ?
-                                                        (
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send-dash" viewBox="0 0 16 16">
-                                                                <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855a.75.75 0 0 0-.124 1.329l4.995 3.178 1.531 2.406a.5.5 0 0 0 .844-.536L6.637 10.07l7.494-7.494-1.895 4.738a.5.5 0 1 0 .928.372l2.8-7Zm-2.54 1.183L5.93 9.363 1.591 6.602l11.833-4.733Z" />
-                                                                <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-5.5 0a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 0-1h-3a.5.5 0 0 0-.5.5" />
-                                                            </svg>
-                                                        )
-                                                        :
-                                                        (
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
-                                                                <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
-                                                            </svg>
-                                                        )
-                                                )
-                                            }
-                                        </button>
+                                        {!codigoNuevoEmailAceptado == true ?
+                                            (<button className="botonVerificarCodigoEmail" onClick={() => verificarCodigo({ codigo: codigoNuevoEmail, email: nuevoEmail })} disabled={codigoNuevoEmailAceptado == true}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
+                                                    <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+                                                </svg>
+                                            </button>)
+                                            :
+                                            (<button className="botonVerificarCodigoEmail">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send-check" viewBox="0 0 16 16">
+                                                    <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855a.75.75 0 0 0-.124 1.329l4.995 3.178 1.531 2.406a.5.5 0 0 0 .844-.536L6.637 10.07l7.494-7.494-1.895 4.738a.5.5 0 1 0 .928.372zm-2.54 1.183L5.93 9.363 1.591 6.602z" />
+                                                    <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686" />
+                                                </svg>
+                                            </button>)}
+
                                     </div>)
                                     :
                                     <button className="botonEnviarCodigoEditarEmail" onClick={() => { enviarCodigo({ email: nuevoEmail, tipo: 'nuevo' }); setAdvertenceMessage(`Código enviado a ${nuevoEmail}`); }}>Enviar código</button>
