d953b8966f25f2fe1f7e4969a51b18ea4155479f
MrPantufla <juanmapizzioo@gmail.com>
2024-01-17 11:52:18 -0300

    Comienzo de navegacion cardCategorias

--- src/Principal/Body/Categorias/cardCategoria.jsx
+++ src/Principal/Body/Categorias/cardCategoria.jsx
@@ -1,13 +1,25 @@
 import './cardCategoria.css';
 import imagen from '../../../Imagenes/categoria.jpg';
+import { useNavigate } from 'react-router-dom';
 
 export default function CardCategoria(args) {
+    const navigate = useNavigate();
+
+    const navegarACategoria = () => {
+        navigate('/tienda');
+        setTimeout(() => {
+            const elemento = document.getElementById('labelPERFIL');
+            elemento.dispatchEvent(new Event('click'));
+        }, 200);
+    }
+
+
     return (
-        <div className="contenedorPrincipalCardCategoria">
+        <div className="contenedorPrincipalCardCategoria" onClick={navegarACategoria}>
             <p className="nombreCategoria">{args.cat}</p>
             <div className="imagenContainerCategoria">
                 <div className="sombraInterna"></div>
-                <img className="imagenCategoria" src={imagen} alt="Imagen de la categoría"/>
+                <img className="imagenCategoria" src={imagen} alt="Imagen de la categoría" />
             </div>
         </div>
     );
