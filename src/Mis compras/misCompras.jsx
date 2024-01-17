f1dec5e093ce38a500b904492210ceb3cca49c56
MrPantufla <juanmapizzioo@gmail.com>
2024-01-17 08:42:31 -0300

    Paginacion misCompras

--- src/Mis compras/misCompras.jsx
+++ src/Mis compras/misCompras.jsx
@@ -11,6 +11,18 @@ import Footer from '../Principal/Footer/footer.jsx';
 export default function MisCompras() {
     const auth = useAuth();
     const [historial, setHistorial] = useState([]);
+    const [paginaActual, setPaginaActual] = useState(1);
+    const itemsPorPagina = 8;
+    const indexUltimoItem = paginaActual * itemsPorPagina;
+    const indexPrimerItem = indexUltimoItem - itemsPorPagina;
+
+    const paginar = (numeroDePagina) => {
+        setPaginaActual(numeroDePagina);
+    }
+
+    const totalPaginas = Math.ceil(historial.length / itemsPorPagina);
+    const numerosDePagina = Array.from({ length: totalPaginas }, (_, index) => index + 1);
+    const itemsActuales = historial.slice(indexPrimerItem, indexUltimoItem);
 
     const navigate = useNavigate();
 
@@ -40,7 +52,7 @@ export default function MisCompras() {
                     }
                     const data = await response.json();
                     console.log(data);
-                    setHistorial(data);
+                    setHistorial(data.reverse());
 
                 } else {
                     console.log('El usuario no está logueado');
@@ -64,12 +76,89 @@ export default function MisCompras() {
                 <div className="decoracionBody decoracionMisCompras" />
                 <div className="decoracionDosBody decoracionDosMisCompras" />
                 <div className="misComprasContainer row">
-                    {historial.slice().reverse().map((item, index) => (
+                    {itemsActuales.slice().map((item, index) => (
                         <div className={`col-6 columnaMisCompras ${index % 2 === 0 ? 'par' : 'impar'}`} key={index} style={{ height: "auto" }}>
                             <CardMisCompras data={item} />
                         </div>
                     ))}
                 </div>
+            </div>
+            <div className="paginacion">
+                <button
+                    className="buttonPag paginaExtremo primeraPagina"
+                    onClick={() => paginar(1)}
+                    disabled={paginaActual === 1}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z" />
+                    </svg>
+                </button>
+                <button
+                    className="botonAntSig buttonPag"
+                    onClick={() => paginar(paginaActual - 1)}
+                    disabled={paginaActual === 1}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-arrow-right"
+                        viewBox="0 0 16 16"
+                        style={{ transform: 'rotate(180deg)' }}
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                        />
+                    </svg>
+                </button>
+                {numerosDePagina.map((numero) => {
+                    const diff = Math.abs(numero - paginaActual);
+
+                    const mostrarPagina = totalPaginas <= 5 || (paginaActual <= 3 && numero <= 5) || (paginaActual >= totalPaginas - 2 && numero >= totalPaginas - 4) || (diff <= 2 && totalPaginas >= 5);
+                    return (
+                        mostrarPagina && (
+                            <button
+                                key={numero}
+                                onClick={() => paginar(numero)}
+                                className={paginaActual === numero ? 'pagina-actual botonPaginacion buttonPag' : 'buttonPag botonPaginacion'}
+                            >
+                                {numero}
+                            </button>
+                        )
+                    );
+                })}
+
+                <button
+                    className="botonAntSig buttonPag"
+                    onClick={() => paginar(paginaActual + 1)}
+                    disabled={indexUltimoItem >= historial.length}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-arrow-right"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                        />
+                    </svg>
+                </button>
+
+                <button
+                    className="buttonPag paginaExtremo ultimaPagina"
+                    onClick={() => paginar(totalPaginas)}
+                    disabled={paginaActual === totalPaginas}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="1.6rem" height="1.6rem" fill="currentColor" className="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z" />
+                    </svg>
+                </button>
             </div>
             <Footer />
         </>
