084cac953f675e72673bddef7a07ae98f2261390
MrPantufla <juanmapizzioo@gmail.com>
2024-01-17 15:14:33 -0300

    Categorias redirigen y filtran

--- src/Principal/Body/Categorias/cardCategoria.jsx
+++ src/Principal/Body/Categorias/cardCategoria.jsx
@@ -1,17 +1,39 @@
 import './cardCategoria.css';
 import imagen from '../../../Imagenes/categoria.jpg';
 import { useNavigate } from 'react-router-dom';
+import { useTienda } from '../../../contextTienda';
 
 export default function CardCategoria(args) {
     const navigate = useNavigate();
+    const tienda = useTienda();
 
     const navegarACategoria = () => {
         navigate('/tienda');
         setTimeout(() => {
-            const elemento = document.getElementById('labelPERFIL');
-            elemento.dispatchEvent(new Event('click'));
-        }, 200);
-    }
+            window.scrollTo(0, 0);
+        }, 100);
+
+        switch (args.cat) {
+            case "PERFILES":
+                tienda.setTiposActivos(["PERFIL"]);
+                break;
+            case "ACCESORIOS":
+                tienda.setTiposActivos(["ACCESORIO"]);
+                break;
+            case "COMPLEMENTOS":
+                tienda.setTiposActivos([""]);
+                break;
+            case "MÁQUINAS Y HERRAMIENTAS":
+                tienda.setTiposActivos(["MAQUINAS", "HERRAMIENTAS"]);
+                break;
+            case "CORTNAS":
+                tienda.setTiposActivos([""]);
+                break;
+            case "SOFTWARE":
+                tienda.setTiposActivos([""]);
+                break;
+        }
+    };
 
 
     return (
