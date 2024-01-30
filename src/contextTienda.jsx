dc69131d67b0c61d4d2b4f510343720749d5bd5d
MrPantufla <juanmapizzioo@gmail.com>
2024-01-30 17:49:38 -0300

    isTablet añadido

--- src/contextTienda.jsx
+++ src/contextTienda.jsx
@@ -10,11 +10,13 @@ function TiendaProvider({ children }) {
   const [tiposActivos, setTiposActivos] = useState([]);
   const [coloresActivos, setColoresActivos] = useState([]);
   const [productoSeleccionado, setProductoSeleccionado] = useState(null);
+  const [isTablet, setIsTablet] = useState(window.innerWidth <= 820);
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   const [isFold, setIsFold] = useState(window.innerWidth <= 280);
 
   useEffect(() => {
     const handleResize = () => {
+      setIsTablet(window.innerWidth <= 820);
       setIsMobile(window.innerWidth < 768);
       setIsFold(window.innerWidth <= 280);
     };
@@ -31,7 +33,7 @@ function TiendaProvider({ children }) {
   }
 
   return (
-    <TiendaContext.Provider value={{ isFold, isMobile, productoSeleccionado, setProductoSeleccionado, limpiarColoresActivos, tiposActivos, setTiposActivos, coloresActivos, setColoresActivos }}>
+    <TiendaContext.Provider value={{isTablet, isFold, isMobile, productoSeleccionado, setProductoSeleccionado, limpiarColoresActivos, tiposActivos, setTiposActivos, coloresActivos, setColoresActivos }}>
       {children}
     </TiendaContext.Provider>
   );
