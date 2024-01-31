3a8023c6912eb68f4eff11fe6746ccfb0fda9174
MrPantufla <juanmapizzioo@gmail.com>
2024-01-31 11:05:21 -0300

    Nueva logica para errorDireccion

--- src/Ventas/Confirmar compra/confirmarCompra.jsx
+++ src/Ventas/Confirmar compra/confirmarCompra.jsx
@@ -1,11 +1,12 @@
 import './confirmarCompra.css';
 import { useDireccion } from '../../contextDireccion';
 import FormularioDireccion from '../../Perfil/Body Perfil/Configuracion/Direcciones/formularioDireccion';
-import CorroborarDatos from './corroborarDatos';
+import CorroborarDatos from './Corroborar datos/corroborarDatos';
 import { useNavigate } from 'react-router-dom';
 import { useConfiguracion } from '../../contextConfiguracion';
 import { useState } from 'react';
 import { useCarrito } from '../../contextCarrito';
+import ErrorDirecion from './Error direccion/errorDireccion';
 
 export default function ConfirmarCompra() {
     const direccion = useDireccion();
@@ -32,11 +33,7 @@ export default function ConfirmarCompra() {
             <div className="parteUtilizableConfirmarCompra" onClick={handleParteUtilizableClick}>
                     {direccion.calle=='' || direccion.numero=='' || direccion.cp=='' || direccion.localidad=='' || direccion.provincia=='' ? 
                     (
-                        <>
-                            <p>No hay una dirección cargada en tu cuenta</p>
-                            <p>Tu carrito se guardará</p>
-                            <button onClick={irAIngresarDireccion}>Ingresar dirección</button>
-                        </>
+                        <ErrorDirecion/>
                     ) 
                     : 
                     (<CorroborarDatos />)}
