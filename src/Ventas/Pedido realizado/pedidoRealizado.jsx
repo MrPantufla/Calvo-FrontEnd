e7cb2230c66cdb056a6b4952bc589a67ad80c3af
MrPantufla <juanmapizzioo@gmail.com>
2024-01-31 11:05:46 -0300

    pedidoRealizado creado

---
+++ src/Ventas/Pedido realizado/pedidoRealizado.jsx
@@ -0,0 +1,23 @@
+import './pedidoRealizado.css';
+import { useCarrito } from '../../contextCarrito';
+
+export default function PedidoRealizado() {
+  const carrito = useCarrito();
+
+  const cerrarVentana = () =>{
+    carrito.setCompraRealizadaAbierto(false);
+  }
+
+  const parteUtilizableClick = (e) =>{
+    e.stopPropagation();
+  }
+
+  return (
+    <div className="contenedorPrincipalPedidoRealizado" onClick={cerrarVentana} style={{ display: carrito.compraRealizadaAbierto ? 'flex' : 'none' }}>
+      <div className="parteUtilizablePedidoRealizado" onClick={parteUtilizableClick}>
+        <h2>Gracias por realizar tu pedido, nos estaremos comunicando en breves</h2>
+        <button onClick={cerrarVentana}>Aceptar</button>
+      </div>
+    </div>
+  );
+}
