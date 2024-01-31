de0af186080ba428bcc930df72dc21c46aa68500
MrPantufla <juanmapizzioo@gmail.com>
2024-01-31 11:06:00 -0300

    PedidoRealizado importado

--- src/Ventas/ventas.jsx
+++ src/Ventas/ventas.jsx
@@ -1,6 +1,5 @@
 import Filtros from './FiltrosYProductos/filtrosYProductos';
 import Footer from '../Principal/Footer/footer.jsx';
-import LoginYRegistro from '../Login y registro/loginYRegistro.jsx';
 import Catalogos from './Catalogos/catalogos.jsx';
 import CartelLogout from '../Logout/cartelLogout.jsx';
 import { useCarrito } from '../contextCarrito.jsx';
@@ -8,6 +7,7 @@ import ConfirmarCompra from './Confirmar compra/confirmarCompra.jsx';
 import DesplegablePerfil from '../Principal/Header/Desplegable perfil/desplegablePerfil.jsx';
 import RenderHeader from '../Principal/Header/renderHeader.jsx';
 import CartelError from './CartelError/cartelError.jsx';
+import PedidoRealizado from './Pedido realizado/pedidoRealizado.jsx';
 
 export default function Ventas() {
   const carrito = useCarrito();
@@ -20,6 +20,7 @@ export default function Ventas() {
       <Filtros />
       {carrito.confirmarCompraAbierto ? (<ConfirmarCompra/>) : (<></>)}
       <CartelError/>
+      <PedidoRealizado/>
       <Footer />
     </>
   );
