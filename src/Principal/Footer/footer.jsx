21ccf0f957f2c56306019d8c3c4da58eee8b4aca
MrPantufla <juanmapizzioo@gmail.com>
2024-02-01 17:43:49 -0300

    cambio de className

--- src/Principal/Footer/footer.jsx
+++ src/Principal/Footer/footer.jsx
@@ -4,8 +4,8 @@ export default function Footer() {
     return (
         <>
             <div className="contenedorPrincipalFooter">
-                <div className="infoContainer">
-                    <p className="textoFooter">Copyright 2024 Calvo Aluminios. Desarrollo: <a href="https://www.linkedin.com/in/juan-manuel-pizzio-2b1477262/">Juan Manuel Pizzio</a> / Diseño: <a href="https://www.instagram.com/impressa_chajari/">IMPRESSA</a></p>
+                <div className="infoFooterContainer">
+                    <p>Copyright 2024 Calvo Aluminios. Desarrollo: <a href="https://www.linkedin.com/in/juan-manuel-pizzio-2b1477262/">Juan Manuel Pizzio</a> / Diseño: <a href="https://www.instagram.com/impressa_chajari/">IMPRESSA</a> - <a href="https://www.linkedin.com/in/juan-manuel-pizzio-2b1477262/">Juan Manuel Pizzio</a></p>
                 </div>
             </div>
         </>
