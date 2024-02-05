07a995a9c046a4a109deffdcfc0ac381c2ac1aa5
MrPantufla <juanmapizzioo@gmail.com>
2024-02-05 17:58:18 -0300

    Responsive ipad air arreglado

--- src/Principal/Body/Informacion y contacto/informacionYContacto.jsx
+++ src/Principal/Body/Informacion y contacto/informacionYContacto.jsx
@@ -12,22 +12,22 @@ export default function InformacionYContacto() {
                     <div className="row containerInfoYContacto">
                         <div className="col-12">
                             <div className="decoracion" >
-                                {!tienda.isMobile ? 'Enviá tu consulta' : 'Información de contacto'}
+                                Enviá tu consulta
                             </div>
                         </div>
                         <div className="col-11 col-sm-6 divInformacion divsInfoYContacto">
-                            {tienda.mobile ? <Contacto /> : <Informacion />}
+                            {tienda.isMobile ? <Contacto /> : <Informacion />}
                         </div>
                         {tienda.isMobile ?
                             <div className="col-12">
                                 <div className="decoracion" >
-                                Enviá tu consulta
+                                Información de contacto
                                 </div>
                             </div>
                             :
                             <></>}
                         <div className="col-11 col-sm-6 divContacto divsInfoYContacto">
-                            {tienda.mobile ? <Informacion /> : <Contacto />}
+                            {tienda.isMobile ? <Informacion /> : <Contacto />}
                         </div>
                     </div>
                 </div>
@@ -45,7 +45,6 @@ export default function InformacionYContacto() {
                         <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
                     </svg>
                 </a>
-
             </div>
         </div>
     );
