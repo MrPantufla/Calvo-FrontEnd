db1d5ceda1acb3c0c9c4bb0aecdfcf1afd42892a
MrPantufla <juanmapizzioo@gmail.com>
2024-02-14 11:52:09 -0300

    .

--- src/Ventas/Cortinas/cortinas.jsx
+++
@@ -1,9 +0,0 @@
-import './cortinas.css';
-
-export default function Cortinas(){
-    return(
-        <div className="contenedorPrincipalCortinas">
-            <h1>asd</h1>
-        </div>
-    );
-}
