23bfc54ae208a2563fd6622525a72149a6c77c3e
MrPantufla <juanmapizzioo@gmail.com>
2024-02-15 16:51:30 -0300

    .

--- src/Ventas/FiltrosYProductos/Cortinas/muestras.jsx
+++
@@ -1,9 +0,0 @@
-import './muestras.css';
-
-export default function Muestras(){
-    return(
-        <div className="contenedorPrincipalMuestras">
-            <h1>asdasd</h1>
-        </div>
-    );
-}
