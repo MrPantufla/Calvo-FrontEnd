7d72cbf4ee1b7549c4926aaf1f8571cc57708b89
MrPantufla <juanmapizzioo@gmail.com>
2024-02-21 17:38:32 -0300

    eliminarProductos arreglado

--- src/Ventas/FiltrosYProductos/Eliminados/eliminados.jsx
+++ src/Ventas/FiltrosYProductos/Eliminados/eliminados.jsx
@@ -1,29 +1,32 @@
 import CardProducto from "../Card Producto/cardProducto";
 import { useProductos } from "../../../contextProductos";
 
-export default function Eliminados(){
-    const { productosEliminados } = useProductos();
+export default function Eliminados() {
+  const {
+    productosEliminados,
+    productosIndexado
+  } = useProductos();
 
-    return(
-        <>
-            <div className="row rowProductos">
-                {productosEliminados.map((producto) => (
-                  <div key={producto.id} className="col-12 col-md-6 col-lg-4 producto">
-                    <CardProducto
-                      id={producto.id}
-                      cod_orig={producto.cod_orig}
-                      tipo_prod={producto.tipo_prod}
-                      srubro={producto.srubro}
-                      detalle={producto.detalle}
-                      precio={producto.precio}
-                      color={producto.color}
-                      kg={producto.kg}
-                      key={producto.id}
-                      cod_int={producto.cod_int}
-                    />
-                  </div>
-                ))}
-              </div>
-        </>
-    );
+  return (
+    <>
+      <div className="row rowProductos">
+        {productosEliminados.map((producto) => (
+          <div key={producto} className="col-12 col-md-6 col-lg-4 producto">
+            <CardProducto
+              id={productosIndexado[producto].id}
+              cod_orig={productosIndexado[producto].cod_orig}
+              tipo_prod={productosIndexado[producto].tipo_prod}
+              srubro={productosIndexado[producto].srubro}
+              detalle={productosIndexado[producto].detalle}
+              precio={productosIndexado[producto].precio}
+              color={productosIndexado[producto].color}
+              kg={productosIndexado[producto].kg}
+              key={productosIndexado[producto].id}
+              cod_int={productosIndexado[producto].cod_int}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
