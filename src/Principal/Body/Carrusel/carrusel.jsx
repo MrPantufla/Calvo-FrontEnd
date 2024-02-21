75b5ebe463bc68c2906c0a1e829c2f1fbadb2c1c
MrPantufla <juanmapizzioo@gmail.com>
2024-02-21 17:37:26 -0300

    Inicio de interfaz cambio de imagenes

--- src/Principal/Body/Carrusel/carrusel.jsx
+++ src/Principal/Body/Carrusel/carrusel.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import './carrusel.css';
-import carrusel1 from '../../../Imagenes/carrusel1.jpg';
 import Carousel from 'react-bootstrap/Carousel';
+import { useAuth } from '../../../contextLogin';
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('../../../Imagenes/ImagenesCarrusel/', false, /\.(png|jpe?g|svg)$/));
+
+const imageKeys = Object.keys(images);
 
 export default function Carrusel() {
+  const { state } = useAuth();
+
   return (
     <div className="container">
-      <Carousel interval={3500}> {/* Cambia el valor de interval según tus necesidades */}
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={carrusel1}
-            alt="Primer slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={carrusel1}
-            alt="Segundo slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={carrusel1}
-            alt="Tercer slide"
-          />
-        </Carousel.Item>
+      <Carousel interval={3500}>
+        {imageKeys.map((imageName, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block w-100"
+              src={images[imageName]}
+              alt={`Slide ${index}`}
+            />
+          </Carousel.Item>
+        ))}
       </Carousel>
     </div>
   );
