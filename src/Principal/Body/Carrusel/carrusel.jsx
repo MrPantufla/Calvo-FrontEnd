d3fd7647e5416e0365aa791c6f0a9f09b6c7ef43
MrPantufla <juanmapizzioo@gmail.com>
2024-02-26 11:49:25 -0300

    Comienzo de implementacion cambio de imagenes

--- src/Principal/Body/Carrusel/carrusel.jsx
+++ src/Principal/Body/Carrusel/carrusel.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './carrusel.css';
 import Carousel from 'react-bootstrap/Carousel';
 import { useAuth } from '../../../contextLogin';
+import { useVariables } from '../../../contextVariables';
 
 function importAll(r) {
   let images = {};
@@ -16,11 +17,35 @@ const imageKeys = Object.keys(images);
 export default function Carrusel() {
   const { state } = useAuth();
 
+  const { backend } = useVariables();
+
+  const intervalo = state.userInfo ? (state.userInfo.tipo_usuario == 'admin' ? null : 2500) : 2500;
+
+  const obtenerUsuarios = async () => {
+    try {
+        const response = await fetch(`${backend}/api/imagenes`, {
+            credentials: 'include'
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            return data;
+        } else {
+            console.error('Error en la solicitud:', response.status);
+            return null;
+        }
+    } catch (error) {
+        console.error('Error desconocido:', error);
+        return null;
+    }
+};
+
   return (
     <div className="container">
-      <Carousel interval={3500}>
+      <Carousel interval={intervalo}>
         {imageKeys.map((imageName, index) => (
-          <Carousel.Item key={index}>
+          <Carousel.Item key={index} >
             <img
               className="d-block w-100"
               src={images[imageName]}
