48808a65fad29ce95fb04e746ace6ff9ec188156
MrPantufla <juanmapizzioo@gmail.com>
2024-03-01 17:12:34 -0300

    div quitado

--- src/Principal/homePage.jsx
+++ src/Principal/homePage.jsx
@@ -2,7 +2,6 @@ import Body from './Body/body.jsx';
 import Footer from './Footer/footer.jsx';
 import DesplegablePerfil from './Header/Desplegable perfil/desplegablePerfil.jsx';
 import RenderHeader from './Header/renderHeader.jsx';
-import './homePage.css';
 import CartelLogout from '../Login y registro/Logout/cartelLogout.jsx';
 import CartelCliente from '../Login y registro/Cartel cliente/cartelCliente.jsx';
 import { useAuth } from '../contextLogin.jsx';
@@ -10,13 +9,13 @@ import { useAuth } from '../contextLogin.jsx';
 export default function HomePage(){
     const { mostrarCartelCliente } = useAuth();
     return(
-        <div className="contenedorHomePage">
+        <>
             <RenderHeader/>
             <DesplegablePerfil/>
             <Body/>
             <Footer/>
             <CartelLogout/>
             {mostrarCartelCliente==true && <CartelCliente/>}
-        </div>
+        </>
     );
 }
