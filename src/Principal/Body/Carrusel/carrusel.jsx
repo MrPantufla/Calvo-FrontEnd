58841879e3997e1e7f3cfb3f49417df616c6ccb9
MrPantufla <juanmapizzioo@gmail.com>
2024-03-05 17:53:19 -0300

    botones subir y eliminar imagenes carrusel

--- src/Principal/Body/Carrusel/carrusel.jsx
+++ src/Principal/Body/Carrusel/carrusel.jsx
@@ -41,25 +41,48 @@ export default function Carrusel() {
     }
   };
 
-  const guardarOEliminarImagen = async () =>{
+  const enviarImagen = async (archivo) => {
     try {
-      const response = await fetch(`${backend}/api/guardarOEliminarImagen`, {
+      const formData = new FormData();
+      formData.append('file', archivo);
+      formData.append('carpeta', "Imagenes carrusel");
+
+      const response = await fetch(`${backend}/api/subirImagen`, {
+        method: 'POST',
+        body: formData,
         credentials: 'include'
       });
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        return data;
-      } else {
-        console.error('Error en la solicitud:', response.status);
-        return null;
-      }
+      console.log(response)
     } catch (error) {
-      console.error('Error desconocido:', error);
-      return null;
+      console.error('Error al intentar subir la imagen:', error);
+      return false;
     }
-  }
+  };
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    enviarImagen(file);
+  };
+
+  const eliminarImagen = async (archivo) => {
+    try {
+      const formData = new FormData();
+      formData.append('imageName', archivo);
+      formData.append('carpeta', "Imagenes carrusel");
+
+      const response = await fetch(`${backend}/api/eliminarImagen`, {
+        method: 'POST',
+        body: formData,
+        credentials: 'include'
+      });
+
+      console.log(response)
+    } catch (error) {
+      console.error('Error al intentar subir la imagen:', error);
+      return false;
+    }
+  };
 
   return (
     <div className="container">
@@ -71,6 +94,20 @@ export default function Carrusel() {
               src={images[imageName]}
               alt={`Slide ${index}`}
             />
+
+            <div className="divSubirArchivoCarrusel storageCarrusel">
+              <label htmlFor="subirImagen" className="boton-personalizado">+</label>
+              <input id="subirImagen" type="file" onChange={handleFileUpload} style={{ display: "none" }} accept=".png, .jpg, .jpeg, .svg"/>
+            </div>
+
+            <div className="divEliminarArchivoCarrusel storageCarrusel">
+              <label htmlFor="eliminarImagen" className="boton-personalizado">
+                <svg xmlns="http://www.w3.org/2000/svg" width="4.5rem" height="4.5rem" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                </svg>
+              </label>
+              <input id="eliminarImagen" onClick={()=> eliminarImagen(imageName)} style={{ display: "none" }} />
+            </div>
           </Carousel.Item>
         ))}
       </Carousel>
