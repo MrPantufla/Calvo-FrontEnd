78e4c4a0b01ac7746da6f8589712071c7e965bb9
MrPantufla <juanmapizzioo@gmail.com>
2024-03-19 10:27:03 -0300

    nuevas categorías agregadas

--- src/Principal/Body/Categorias/categorias.jsx
+++ src/Principal/Body/Categorias/categorias.jsx
@@ -2,13 +2,13 @@ import CardCategoria from './cardCategoria';
 import './categorias.css';
 
 export default function Categorias() {
-    const categorias = ["PERFILES", "ACCESORIOS", "COMPLEMENTOS", "MÁQUINAS Y HERRAMIENTAS", "CORTINAS", "SOFTWARE"];
+    const categorias = ["PERFILES", "ACCESORIOS", "PANELES", "PUERTAS PLACAS", "POLICARBONATOS Y POLIESTIRENOS", "MÁQUINAS Y HERRAMIENTAS", "CORTINAS", "SOFTWARE"];
     
     return (
         <div className="contenedorPrincipalCategorias">
             <div className="row rowCategorias">
                 {categorias.map((categoria, index) => (
-                    <div className="col-6 col-md-4 categoria" key={categoria}>
+                    <div className="col-6 col-md-3 categoria" key={categoria}>
                         <CardCategoria cat={categoria} imagen={(index % 2 !== 0) ? ("ACCESORIO2") : ("PERFIL")}/>
                     </div>
                 ))}
