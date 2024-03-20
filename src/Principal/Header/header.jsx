b235db5e2493178330bc37bdf7248edbbcaaf95e
MrPantufla <juanmapizzioo@gmail.com>
2024-03-20 14:44:23 -0300

    nombre en ícono de perfil

--- src/Principal/Header/header.jsx
+++ src/Principal/Header/header.jsx
@@ -145,12 +145,19 @@ export default function Header() {
             id="perfilHeader"
             className={`perfil ${desplegablePerfil.perfilHovered ? 'perfilHovered' : ''}`} onMouseEnter={desplegablePerfil.abrirPerfil} onMouseLeave={desplegablePerfil.cerrarPerfil}
             style={{ width: location.pathname === '/tienda' ? '15rem' : '23rem' }}
-          >
-            <div className="iconoContainer">
+          >{state.logueado ?
+            (<p>
+              <svg xmlns="http://www.w3.org/2000/svg" width="2.5rem" height="2.5rem" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+              </svg> {state.userInfo.nombre.toUpperCase()}
+            </p>)
+            :
+            (<div className="iconoContainer">
               <svg xmlns="http://www.w3.org/2000/svg" width="2.5rem" height="2.5rem" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
               </svg>
-            </div>
+            </div>)}
+
           </div>
           {location.pathname == '/tienda' ? (
             <>
