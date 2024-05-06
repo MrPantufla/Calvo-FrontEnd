cfe512b58829c965b4991b324f6354c75da07bc7
MrPantufla <juanmapizzioo@gmail.com>
2024-05-06 18:30:08 -0300

    correción de color menu en iphone

--- src/Principal/Header/HeaderMobile.jsx
+++ src/Principal/Header/HeaderMobile.jsx
@@ -126,11 +126,10 @@ export default function HeaderMobile() {
 
     return (
         <header className="containerGeneralHeaderMobile">
-            {/*<div className="overlay" onClick={toggleMenu} />*/}
             <div className="containerBotonYLogoMobile">
                 <div className="containerBotonMobile">
                     <button className="botonHeader" id="toggleHeaderButton" onClick={toggleMenu}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="black" className="bi bi-list" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                         </svg>
                     </button>
