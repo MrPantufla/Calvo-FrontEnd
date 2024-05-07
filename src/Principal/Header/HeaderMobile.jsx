d56df2a383debf8b3d36741c577b9e529a0602bc
MrPantufla <juanmapizzioo@gmail.com>
2024-05-07 15:13:36 -0300

    botonHeader mejorado

--- src/Principal/Header/HeaderMobile.jsx
+++ src/Principal/Header/HeaderMobile.jsx
@@ -129,7 +129,7 @@ export default function HeaderMobile() {
             <div className="containerBotonYLogoMobile">
                 <div className="containerBotonMobile">
                     <button className="botonHeader" id="toggleHeaderButton" onClick={toggleMenu}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="black" className="bi bi-list" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" className="bi bi-list" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                         </svg>
                     </button>
