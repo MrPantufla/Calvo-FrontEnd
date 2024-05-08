e250048ad2bd3d265baf7366172c1be82bc596c8
MrPantufla <juanmapizzioo@gmail.com>
2024-05-08 18:40:14 -0300

    .

--- src/Principal/Header/HeaderMobile.jsx
+++ src/Principal/Header/HeaderMobile.jsx
@@ -197,7 +197,6 @@ export default function HeaderMobile() {
                             </>
                             )}
                     </div>
-
                 </div>
             </div>
         </header>
