983c55144e2e18cdff4ad2d35dad529469a666f7
MrPantufla <juanmapizzioo@gmail.com>
2024-05-15 18:55:13 -0300

    Categorías funcionales al 100%

--- src/Principal/Body/Categorias/cardCategoria.jsx
+++ src/Principal/Body/Categorias/cardCategoria.jsx
@@ -1,15 +1,12 @@
 import './cardCategoria.css';
-import imagen from '../../../Imagenes/categoria.jpg';
 import { useNavigate } from 'react-router-dom';
 import { useTienda } from '../../../contextTienda';
 
 export default function CardCategoria(args) {
     const navigate = useNavigate();
     const {
-        setTiposActivos,
-        seleccionarCortinas,
-        setCortinasSelected,
-        setEliminadosSelected
+        togglearRubro,
+        seleccionarCortinas
     } = useTienda();
 
     const navegarACategoria = () => {
@@ -18,37 +15,8 @@ export default function CardCategoria(args) {
             window.scrollTo(0, 0);
         }, 100);
 
-        switch (args.cat) {
-            case "PERFILES":
-                setCortinasSelected(false);
-                setEliminadosSelected(false);
-                setTiposActivos(["PERFIL"]);
-                break;
-            case "ACCESORIOS":
-                setCortinasSelected(false);
-                setEliminadosSelected(false);
-                setTiposActivos(["ACCESORIO"]);
-                break;
-            case "COMPLEMENTOS":
-                setCortinasSelected(false);
-                setEliminadosSelected(false);
-                setTiposActivos([""]);
-                break;
-            case "MÁQUINAS Y HERRAMIENTAS":
-                setCortinasSelected(false);
-                setEliminadosSelected(false);
-                setTiposActivos(["MAQUINAS", "HERRAMIENTAS"]);
-                break;
-            case "CORTINAS":
-                seleccionarCortinas();
-                setEliminadosSelected(false);
-                break;
-            case "SOFTWARE":
-                setCortinasSelected(false);
-                setEliminadosSelected(false);
-                setTiposActivos([""]);
-                break;
-        }
+        args.cat == 'POLICARBONATOS Y POLIESTIRENOS' ? (togglearRubro(31)) : (togglearRubro(args.referencia));
+        args.cat == 'CORTINAS' && (seleccionarCortinas())
     };
 
 
--- src/Principal/Body/Categorias/categorias.jsx
+++ src/Principal/Body/Categorias/categorias.jsx
@@ -1,17 +1,30 @@
 import CardCategoria from './cardCategoria';
 import './categorias.css';
+import { rubros } from '../../../rubros.js';
 
 export default function Categorias() {
-    const categorias = ["PERFILES", "ACCESORIOS", "PANELES", "PUERTAS PLACAS", "POLICARBONATOS Y POLIESTIRENOS", "MÁQUINAS Y HERRAMIENTAS", "CORTINAS", "SOFTWARE"];
-    
+    const categorias = ["PERFILES", "ACCESORIOS", "AUTOMATISMOS", "CHAPAS", "HERRAMIENTAS", "PANELES", "POLICARBONATOS Y POLIESTIRENOS", "PUERTAS PLACAS", "TEJIDOS MOSQUITEROS", "MÁQUINAS", "CORTINAS", "SOFTWARE"];
+
     return (
         <div className="contenedorPrincipalCategorias">
             <div className="row rowCategorias">
-                {categorias.map((categoria, index) => (
-                    <div className="col-6 col-md-3 categoria" key={categoria}>
-                        <CardCategoria cat={categoria} imagen={(index % 2 !== 0) ? ("ACCESORIO2") : ("PERFIL")}/>
-                    </div>
-                ))}
+                {categorias.map((categoria, index) => {
+                    let rubro = rubros.find(r => r.nombre === categoria);
+
+                    if (categoria === 'POLICARBONATOS Y POLIESTIRENOS') {
+                        rubro = rubros.find(r => r.nombre === "POLICARBONATOS");
+                    }
+
+                    return (
+                        <div className="col-6 col-md-3 categoria" key={categoria}>
+                            <CardCategoria
+                                referencia={rubro ? rubro.id : null}
+                                cat={categoria}
+                                imagen={(index % 2 !== 0) ? "ACCESORIO2" : "PERFIL"}
+                            />
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
