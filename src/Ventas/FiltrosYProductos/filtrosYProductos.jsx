f204f4db10616e9a8a765bbd69fbf6c5526d0a56
MrPantufla <juanmapizzioo@gmail.com>
2024-05-24 16:56:10 -0300

    .

--- src/Ventas/FiltrosYProductos/filtrosYProductos.jsx
+++ src/Ventas/FiltrosYProductos/filtrosYProductos.jsx
@@ -370,96 +370,97 @@ export default function FiltrosYProductos() {
         />
       )}
 
-      {(cortinasSelected || eliminadosSelected) ? (<></>) : (<div className="paginacion">
-        <button
-          className="buttonPag paginaExtremo primeraPagina"
-          onClick={() => paginar(1)}
-          disabled={paginaActual === 1}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" width="1.6rem" height="1.6rem" fillRule="currentColor" className="bi bi-arrow-bar-left" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z" />
-          </svg>
-        </button>
-        <button
-          className="botonAntSig buttonPag"
-          onClick={() => paginar(paginaActual - 1)}
-          disabled={paginaActual === 1}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1.6rem"
-            height="1.6rem"
-            fill="currentColor"
-            className="bi bi-arrow-right"
-            viewBox="0 0 16 16"
-            style={{ transform: 'rotate(180deg)' }}
+      {(cortinasSelected || eliminadosSelected) ? (<></>) : (
+        <div className="paginacion">
+          <button
+            className="buttonPag paginaExtremo primeraPagina"
+            onClick={() => paginar(1)}
+            disabled={paginaActual === 1}
           >
-            <path
-              fillRule="evenodd"
-              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-            />
-          </svg>
-        </button>
-        {numerosDePagina.map((numero) => {
-          const diff = Math.abs(numero - paginaActual);
-
-          let mostrarPagina
-          if (isMobile) {
-            // Mostrar solo 3 botones en dispositivos móviles
-            mostrarPagina =
-              (paginaActual <= totalPaginas - 2 && numero >= paginaActual && numero <= paginaActual + 2) ||
-              (paginaActual > totalPaginas - 2 && numero >= totalPaginas - 2);
-          } else {
-            // Lógica para mostrar botones según el caso actual
-            mostrarPagina = totalPaginas <= 5 ||
-              (paginaActual <= 3 && numero <= 5) ||
-              (paginaActual >= totalPaginas - 2 && numero >= totalPaginas - 4) ||
-              (diff <= 2 && totalPaginas >= 5);
-          }
-
-          return (
-            mostrarPagina && (
-              <button
-                key={numero}
-                onClick={() => paginar(numero)}
-                className={paginaActual === numero ? 'pagina-actual botonPaginacion buttonPag' : 'buttonPag botonPaginacion'}
-              >
-                {numero}
-              </button>
-            )
-          );
-        })}
-
-        <button
-          className="botonAntSig buttonPag"
-          onClick={() => paginar(paginaActual + 1)}
-          disabled={indexUltimoItem >= listaFiltrada.length}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1.6rem"
-            height="1.6rem"
-            fill="currentColor"
-            className="bi bi-arrow-right"
-            viewBox="0 0 16 16"
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.6rem" height="1.6rem" fillRule="currentColor" className="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z" />
+            </svg>
+          </button>
+          <button
+            className="botonAntSig buttonPag"
+            onClick={() => paginar(paginaActual - 1)}
+            disabled={paginaActual === 1}
           >
-            <path
-              fillRule="evenodd"
-              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-            />
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.6rem"
+              height="1.6rem"
+              fill="currentColor"
+              className="bi bi-arrow-right"
+              viewBox="0 0 16 16"
+              style={{ transform: 'rotate(180deg)' }}
+            >
+              <path
+                fillRule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+              />
+            </svg>
+          </button>
+          {numerosDePagina.map((numero) => {
+            const diff = Math.abs(numero - paginaActual);
 
-        <button
-          className="buttonPag paginaExtremo ultimaPagina"
-          onClick={() => paginar(totalPaginas)}
-          disabled={paginaActual === totalPaginas}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" width="1.6rem" height="1.6rem" fill="currentColor" className="bi bi-arrow-bar-left" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z" />
-          </svg>
-        </button>
-      </div>)}
+            let mostrarPagina
+            if (isMobile) {
+              // Mostrar solo 3 botones en dispositivos móviles
+              mostrarPagina =
+                (paginaActual <= totalPaginas - 2 && numero >= paginaActual && numero <= paginaActual + 2) ||
+                (paginaActual > totalPaginas - 2 && numero >= totalPaginas - 2);
+            } else {
+              // Lógica para mostrar botones según el caso actual
+              mostrarPagina = totalPaginas <= 5 ||
+                (paginaActual <= 3 && numero <= 5) ||
+                (paginaActual >= totalPaginas - 2 && numero >= totalPaginas - 4) ||
+                (diff <= 2 && totalPaginas >= 5);
+            }
+
+            return (
+              mostrarPagina && (
+                <button
+                  key={numero}
+                  onClick={() => paginar(numero)}
+                  className={paginaActual === numero ? 'pagina-actual botonPaginacion buttonPag' : 'buttonPag botonPaginacion'}
+                >
+                  {numero}
+                </button>
+              )
+            );
+          })}
+
+          <button
+            className="botonAntSig buttonPag"
+            onClick={() => paginar(paginaActual + 1)}
+            disabled={indexUltimoItem >= listaFiltrada.length}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.6rem"
+              height="1.6rem"
+              fill="currentColor"
+              className="bi bi-arrow-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+              />
+            </svg>
+          </button>
+
+          <button
+            className="buttonPag paginaExtremo ultimaPagina"
+            onClick={() => paginar(totalPaginas)}
+            disabled={paginaActual === totalPaginas}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="1.6rem" height="1.6rem" fill="currentColor" className="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z" />
+            </svg>
+          </button>
+        </div>)}
     </div>
   );
 }
