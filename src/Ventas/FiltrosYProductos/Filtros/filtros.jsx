06d5199cf94cb60d46d21a1402a6bdd39e75ad68
MrPantufla <juanmapizzioo@gmail.com>
2024-06-06 08:56:27 -0300

    Boton filtros utilizado para guardar destacados

--- src/Ventas/FiltrosYProductos/Filtros/filtros.jsx
+++ src/Ventas/FiltrosYProductos/Filtros/filtros.jsx
@@ -3,6 +3,7 @@ import { useTienda } from '../../../contextTienda.jsx';
 import Rubros from './Rubros/rubros.jsx';
 import { useEffect, useState } from 'react';
 import './filtros.css';
+import { useProductos } from '../../../contextProductos.jsx';
 
 export default function Filtros(args) {
     const { state } = useAuth();
@@ -15,6 +16,8 @@ export default function Filtros(args) {
         seleccionarEliminados,
         rubroActivo
     } = useTienda();
+
+    const { guardarDestacados } = useProductos();
 
     const [scrollDownFiltros, setScrollDownFiltros] = useState(false);
 
@@ -55,16 +58,30 @@ export default function Filtros(args) {
     return (
         <>
             <div className='filtros' id='filtros'>
-                <Rubros setPaginaActual={args.setPaginaActual} coloresUnicos={args.coloresUnicos} srubrosUnicos={args.srubrosUnicos}/>
-                
+                <Rubros setPaginaActual={args.setPaginaActual} coloresUnicos={args.coloresUnicos} srubrosUnicos={args.srubrosUnicos} />
+
                 <div className={`labelRubros ${cortinasSelected ? 'checked' : ''} textoLabelRubros ${(state.userInfo && state.userInfo.tipo_usuario !== 'admin') && 'ultimoLabel'}`} onClick={() => seleccionarCortinas()}>CORTINAS</div>
                 {state.userInfo && (state.userInfo.tipo_usuario == 'admin' && (<div className={`labelRubros ${eliminadosSelected ? 'checked' : ''} textoLabelRubros ultimoLabel`} onClick={() => seleccionarEliminados()}>ELIMINADOS</div>))}
             </div>
             {!isMobile &&
-                <div className={`scrollerFiltros ${scrollDownFiltros ? 'enabled' : 'disabled'}`} onClick={scrollearFiltros}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" fill="currentColor" className="bi bi-arrow-down-short" viewBox="0 0 16 16">
-                        <path fillRule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4" />
-                    </svg>
+                <div className={`scrollerFiltros ${scrollDownFiltros ? 'enabled' : 'disabled'} ${state.userInfo ? (state.userInfo.tipo_usuario == 'admin' && 'admin') : ('')}`} onClick={state.userInfo && (state.userInfo.tipo_usuario == 'admin' ? (guardarDestacados) : (scrollearFiltros))}>
+                    {state.userInfo ? (state.userInfo.tipo_usuario == 'admin' ?
+                        (<>
+                            Guardar destacados
+                            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="currentColor" className="bi bi-box-arrow-down guardarDestacados" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M3.5 10a.5.5 0 0 1-.5-.5v-8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 0 0 1h2A1.5 1.5 0 0 0 14 9.5v-8A1.5 1.5 0 0 0 12.5 0h-9A1.5 1.5 0 0 0 2 1.5v8A1.5 1.5 0 0 0 3.5 11h2a.5.5 0 0 0 0-1z" />
+                                <path fillRule="evenodd" d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708z" />
+                            </svg>
+                        </>)
+                        :
+                        (<svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" fill="currentColor" className="bi bi-arrow-down-short" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4" />
+                        </svg>)
+                    )
+                        :
+                        (<svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" fill="currentColor" className="bi bi-arrow-down-short" viewBox="0 0 16 16">
+                            <path fillRule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4" />
+                        </svg>)}
                 </div>
             }
         </>
