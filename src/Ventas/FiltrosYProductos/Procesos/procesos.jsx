7a01e60b92ec27580c95b891d1b57936db00f607
MrPantufla <juanmapizzioo@gmail.com>
2024-06-11 18:52:54 -0300

    Logica para eliminar procesos con administrador

--- src/Ventas/FiltrosYProductos/Procesos/procesos.jsx
+++ src/Ventas/FiltrosYProductos/Procesos/procesos.jsx
@@ -4,6 +4,7 @@ import CardProducto from "../Card Producto/cardProducto";
 import { useProductos } from '../../../contextProductos';
 import { useEffect } from 'react';
 import { marcasPerfiles } from '../../../rubros';
+import { useAuth } from '../../../contextLogin';
 
 export default function Procesos(args) {
     const {
@@ -17,21 +18,26 @@ export default function Procesos(args) {
         togglearProceso
     } = useTienda();
 
-    useEffect(() => {
-        console.log(stipoProceso)
-    }, [stipoProceso])
-
     const {
-        productosIndexado
+        productosIndexado,
+        eliminarProducto,
+        productosEliminados
     } = useProductos();
 
-    const { procesosRenderizar } = useProductos();
+    const { state } = useAuth();
+
+    const { procesos } = useProductos();
+
+    const eliminarProceso = (e, id) => {
+        e.stopPropagation();
+        eliminarProducto(id);
+    }
 
     return (
         <div className="contenedorPrincipalProcesos">
             {tipoProceso == null ?
                 (<div className="tiposProcesoContainer">
-                    <div className="tipoProceso" onClick={() => { setTipoProceso('anodizados'); console.log(procesosRenderizar) }}>
+                    <div className="tipoProceso" onClick={() => setTipoProceso('anodizados')}>
                         <h1>ANODIZADOS</h1>
                     </div>
 
@@ -43,42 +49,55 @@ export default function Procesos(args) {
                 (<>
                     <div className="row rowProductos">
                         {stipoProceso == null ?
-                            (tipoProceso == 'anodizados' ?
-                                (Object.values(procesosRenderizar).map((proceso) => {
-                                    if (proceso.rubro === 88) {
+                            (Object.values(procesos).map((proceso) => {
+                                if (!productosEliminados.includes(proceso.id)) {
+                                    const colorCorregido = (proceso.color).replace(/\s+/g, '-');
+                                    if (tipoProceso == 'anodizados' && proceso.rubro === 88) {
                                         return (
                                             <div key={proceso.id} className="col-12 col-md-6 col-lg-4 producto proceso">
-                                                <div className="cardProceso" onClick={() => setStipoProceso(proceso)}>
+                                                <div className="cardProceso" onClick={() => setStipoProceso(proceso)} style={{ backgroundColor: `var(--${colorCorregido})` }}>
+                                                    {state.userInfo && state.userInfo.tipo_usuario === 'admin' &&
+                                                        <button className="eliminarElemento" onClick={(e) => eliminarProceso(e, proceso.id)}>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                                                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                                                            </svg>
+                                                        </button>
+                                                    }
+
                                                     <h1>{proceso.detalle}</h1>
                                                 </div>
                                             </div>
                                         );
                                     }
-                                    return null;
-                                }))
-                                :
-                                (Object.values(procesosRenderizar).map((proceso) => {
-                                    if (proceso.rubro !== 88) {
+                                    else if (tipoProceso == 'pinturas' && proceso.rubro !== 88) {
                                         return (
-                                            <div key={proceso.id} className="col-12 col-md-6 col-lg-4 producto proceso" onClick={() => setStipoProceso(proceso)}>
-                                                <div className="cardProceso">
+                                            <div key={proceso.id} className="col-12 col-md-6 col-lg-4 producto proceso">
+                                                <div className="cardProceso" onClick={() => setStipoProceso(proceso)} style={{ backgroundColor: `var(--${colorCorregido})` }}>
+                                                    {state.userInfo && state.userInfo.tipo_usuario === 'admin' &&
+                                                        <button className="eliminarElemento" onClick={(e) => eliminarProceso(e, proceso.id)}>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                                                <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                                                            </svg>
+                                                        </button>
+                                                    }
+
                                                     <h1>{proceso.detalle}</h1>
                                                 </div>
                                             </div>
                                         );
                                     }
                                     return null;
-                                }))
-                            )
+                                }
+                            }))
                             :
                             (Object.values(args.itemsActuales).map((producto) => (
                                 <div key={producto.id} className="col-12 col-md-6 col-lg-4 producto">
                                     <CardProducto
                                         producto={producto}
                                         color={stipoProceso.color}
-                                        anodizado={stipoProceso == 'anodizado' && true}
+                                        proceso={stipoProceso.id}
                                         onClick={() => {
-                                            seleccionarProducto(producto);
+                                            args.seleccionarProducto(producto);
                                         }}
                                     />
                                 </div>
