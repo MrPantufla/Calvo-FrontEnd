26d2f99832625d92c8e530e8b1de167c7d5e0afc
MrPantufla <juanmapizzioo@gmail.com>
2024-06-11 18:54:07 -0300

    Logica de procesos no terminada

--- src/Ventas/Carrito/cardCarrito.jsx
+++ src/Ventas/Carrito/cardCarrito.jsx
@@ -17,12 +17,23 @@ export default function CardCarrito(args) {
 
     const { productosSueltos } = useProductos();
 
+    const extraerAntesDeParentesis = (cadena) => {
+        const match = cadena.match(/^([^()]*)\(/);
+        if (match) {
+            return parseInt(match[1]);
+        }
+        return parseInt(cadena);
+    }
+
+    const idSinProceso = extraerAntesDeParentesis(args.id.toString());
+
     let producto;
 
-    producto = productosIndexado[args.id];
+    producto = productosIndexado[idSinProceso];
     if (!producto) {
-        producto = productosSueltos[args.id];
+        producto = productosSueltos[idSinProceso];
     }
+
     const colorCorregido = (producto.color).replace(/\s+/g, '-');
 
     let codigo;
@@ -62,7 +73,7 @@ export default function CardCarrito(args) {
                 </div>
                 <p className="kgCardCarrito">{producto.kg > 0 ? ('- ' + producto.kg + 'kg -') : ('')}</p>
                 <div className="codigoYDetalleCardCarritoContainer">
-                    <p className="codigoYDetalleCardCarrito"><span>{producto.cod_orig}</span> - {producto.detalle} <span className="codOrig">{`${producto.cantidad > 1 ? ('(' + producto.cantidad + 'u.)') : ('')}`}</span></p>
+                    <p className="codigoYDetalleCardCarrito"><span>{producto.id}</span> - {producto.detalle} <span className="codOrig">{`${producto.cantidad > 1 ? ('(' + producto.cantidad + 'u.)') : ('')}`}</span></p>
                 </div>
             </div>
             <div className="restoCardCarrito">
@@ -71,8 +82,8 @@ export default function CardCarrito(args) {
                     <div>
                         <p>{args.cantidadCarrito}</p>
                     </div>
-                    <button className="botonSumarCardCarrito" onClick={() => añadirElemento(args.id, 1)}>+</button>
-                    <p className="eliminarCardCarrito" onClick={() => eliminarElemento(args.id)}><svg xmlns="http://www.w3.org/2000/svg" width="1.7rem" height="1.7rem" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
+                    <button className="botonSumarCardCarrito" onClick={() => añadirElemento(idSinProceso, 1)}>+</button>
+                    <p className="eliminarCardCarrito" onClick={() => eliminarElemento(idSinProceso)}><svg xmlns="http://www.w3.org/2000/svg" width="1.7rem" height="1.7rem" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
                     </svg></p>
                 </div>
