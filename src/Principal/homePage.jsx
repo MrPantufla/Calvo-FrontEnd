5acedcc1a26c7a635ac7751e772dfd89168b2912
MrPantufla <juanmapizzioo@gmail.com>
2024-06-27 18:46:49 -0300

    CartelCliente movido a app.js

--- src/Principal/homePage.jsx
+++ src/Principal/homePage.jsx
@@ -3,13 +3,11 @@ import Footer from './Footer/footer.jsx';
 import DesplegablePerfil from './Header/Desplegable perfil/desplegablePerfil.jsx';
 import RenderHeader from './Header/renderHeader.jsx';
 import CartelLogout from '../Login y registro/Logout/cartelLogout.jsx';
-import CartelCliente from '../Login y registro/Cartel cliente/cartelCliente.jsx';
 import { useAuth } from '../contextLogin.jsx';
 import { useTienda } from '../contextTienda.jsx';
 import { useEffect } from 'react';
 
 export default function HomePage() {
-    const { mostrarCartelCliente } = useAuth();
     const { salirDeTienda } = useTienda();
 
     useEffect(() => {
@@ -23,7 +21,6 @@ export default function HomePage() {
             <Body />
             <Footer />
             <CartelLogout />
-            {mostrarCartelCliente == true && <CartelCliente />}
         </>
     );
 }
