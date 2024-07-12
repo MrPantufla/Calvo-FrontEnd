670688f99888d908f2349942d25ca3ca35ed98df
MrPantufla <juanmapizzioo@gmail.com>
2024-07-12 17:46:11 -0300

    arreglos incompletos de estilo error

--- src/Login y registro/Login/login.jsx
+++ src/Login y registro/Login/login.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './login.css';
-import Cookies from 'js-cookie'
 import { useAuth } from '../../contextLogin.jsx';
 
 export default function Login() {
@@ -45,9 +44,9 @@ export default function Login() {
   return (
     <div className="login-container">
       <div className="errorLogin errorFormulario">
-        {errorMessage != ('') ? (<svg xmlns="http://www.w3.org/2000/svg" width="1.3rem" height="1.3rem" fill="var(--colorRojo)" className="bi bi-exclamation-diamond-fill" viewBox="0 0 16 16">
+        {errorMessage != ('') ? (<><svg xmlns="http://www.w3.org/2000/svg" width="1.3rem" height="1.3rem" fill="var(--colorRojo)" className="bi bi-exclamation-diamond-fill" viewBox="0 0 16 16">
           <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-        </svg>) : (<></>)}{errorMessage}
+        </svg> {errorMessage}</>) : (errorMessage)}
       </div>
       <form className="formularioLogin" id="formularioLogin">
         <div className="form-group inputFormularioLogin">
