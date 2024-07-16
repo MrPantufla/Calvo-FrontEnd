c5572c2f3b20ac4a30818836d82915fc79298561
MrPantufla <juanmapizzioo@gmail.com>
2024-07-16 18:48:35 -0300

    Navegacion quitada

--- src/Perfil/perfil.jsx
+++ src/Perfil/perfil.jsx
@@ -2,31 +2,9 @@ import './perfil.css';
 import BodyPerfil from './Body Perfil/bodyPerfil';
 import Footer from '../Principal/Footer/footer.jsx';
 import DesplegablePerfil from '../Principal/Header/Desplegable perfil/desplegablePerfil.jsx';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useAuth } from '../contextLogin.jsx';
 import RenderHeader from '../Principal/Header/renderHeader.jsx';
-import { useTienda } from '../contextTienda.jsx';
 
 export default function Perfil() {
-    const navigate = useNavigate();
-    const {state} = useAuth();
-    const { salirDeTienda } = useTienda();
-
-    useEffect(() => {
-        salirDeTienda();
-    }, [])
-
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            if (!state.logueado || !state.userInfo.email_confirmado) {
-                navigate("/");
-            }
-        }, 200);
-
-        return () => clearTimeout(timeoutId);
-    });
-
     return (
         <div className="contenedorPrincipalPerfil">
             <RenderHeader/>
