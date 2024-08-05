ffa6d2c10a13ec36e3e333213acc300105af0417
MrPantufla <juanmapizzioo@gmail.com>
2024-08-05 07:41:07 -0300

    providers movidos de index a app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import HomePage from './Principal/homePage';
 import Ventas from './Ventas/ventas.jsx';
 import Perfil from './Perfil/perfil.jsx';
+import { LoginProvider } from './contextLogin.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { DesplegableProviderCatalogos } from './contextDesplegableCatalogos.jsx';
 import MisCompras from './Mis compras/misCompras.jsx';
@@ -12,28 +13,43 @@ import EditarUsuarios from './Editar usuarios/editarUsuarios.jsx';
 import LoginYRegistro from './Login y registro/loginYRegistro.jsx';
 import { useAuth } from './contextLogin.jsx';
 import CartelCliente from './Login y registro/Cartel cliente/cartelCliente.jsx';
+import { CarritoProvider } from './contextCarrito.jsx';
+import { FavoritosProvider } from './contextFavoritos.jsx';
+import { ProductosProvider } from './contextProductos.jsx';
+import { ConfiguracionProvider } from './contextConfiguracion.jsx';
+import { useVariables } from './contextVariables.jsx';
 
 function App() {
-  const { mostrarCartelCliente } = useAuth();
+  const { mostrarCartelCliente } = useVariables();
 
   return (
-    <DesplegableProviderCatalogos>
-      <DesplegableProviderPerfil>
-        <Router>
-          <TiendaProvider>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/tienda" element={<Ventas />} />
-              <Route path="/perfil" element={<Perfil />} />
-              <Route path="/misCompras" element={<MisCompras />} />
-              <Route path="/editarUsuarios" element={<EditarUsuarios />} />
-            </Routes>
-            {mostrarCartelCliente == true && <CartelCliente />}
-          </TiendaProvider>
-        </Router>
-        <LoginYRegistro />
-      </DesplegableProviderPerfil>
-    </DesplegableProviderCatalogos>
+    <ProductosProvider>
+      <DesplegableProviderCatalogos>
+        <DesplegableProviderPerfil>
+          <Router>
+            <LoginProvider>
+              <CarritoProvider>
+                <FavoritosProvider>
+                  <TiendaProvider>
+                    <ConfiguracionProvider>
+                      <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/tienda" element={<Ventas />} />
+                        <Route path="/perfil" element={<Perfil />} />
+                        <Route path="/misCompras" element={<MisCompras />} />
+                        <Route path="/editarUsuarios" element={<EditarUsuarios />} />
+                      </Routes>
+                      {mostrarCartelCliente == true && <CartelCliente />}
+                      <LoginYRegistro />
+                    </ConfiguracionProvider>
+                  </TiendaProvider>
+                </FavoritosProvider>
+              </CarritoProvider>
+            </LoginProvider>
+          </Router>
+        </DesplegableProviderPerfil>
+      </DesplegableProviderCatalogos>
+    </ProductosProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,26 +4,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { CarritoProvider } from './contextCarrito.jsx';
-import { FavoritosProvider } from './contextFavoritos.jsx';
-import { LoginProvider } from './contextLogin.jsx';
-import { ProductosProvider } from './contextProductos.jsx';
 import { VariablesProvider } from './contextVariables.jsx';
-import { ConfiguracionProvider } from './contextConfiguracion.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <VariablesProvider>
-    <ProductosProvider>
-      <LoginProvider>
-        <ConfiguracionProvider>
-            <CarritoProvider>
-              <FavoritosProvider>
-                <App />
-              </FavoritosProvider>
-            </CarritoProvider>
-        </ConfiguracionProvider>
-      </LoginProvider>
-    </ProductosProvider>
+    <App />
   </VariablesProvider>
 );
