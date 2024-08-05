798bfc8b683892e40f1d29408d24ee8bc7474f1b
MrPantufla <juanmapizzioo@gmail.com>
2024-08-05 07:38:31 -0300

    Nuevo texto y estilos más claros

--- src/Principal/Body/Quienes somos/quienesSomos.jsx
+++ src/Principal/Body/Quienes somos/quienesSomos.jsx
@@ -4,18 +4,10 @@ import CAquienesSomos from '../../../Imagenes/CAquienesSomos.webp';
 
 export default function QuienesSomos() {
 
-    const message = "Fundada en 1994 en Chajarí - Entre Ríos, somos una empresa con amplia trayectoria que se ha destacado siempre por su enfoque en el servicio al cliente. \nEntre nuestros valores destacan la honestidad, confianza, responsabilidad y compromiso que ofrecemos tanto dentro del ámbito laboral como con nuestros proveedores y clientes. \n Nos encargamos de la comercialización y distribución de insumos para la carpintería de aluminio y contamos con más de 400 clientes a lo largo de las provincias de Entre Ríos, Corrientes y Misiones, a los que con un sistema de transporte propio, les somos garantía de una calidad de servicio y productos sobresaliente.";
+    const message = "Somos una empresa familiar, fundada en 1994 en Chajarí, Entre Ríos. Nos hemos consolidado como un referente en la distribución de perfiles y todos los  insumos necesarios para la fabricación de aberturas de aluminio.  A lo largo de nuestra extensa trayectoria, nos hemos destacado por nuestro enfoque en el servicio al cliente y la comercializacion de productos de calidad.\nNuestros valores esenciales, como la honestidad, confianza, responsabilidad y compromiso, se reflejan en cada aspecto de nuestras operaciones, tanto internamente como en nuestras relaciones con proveedores y clientes. \nAtendemos a más de 400 clientes en las provincias de Entre Ríos, Corrientes y Misiones, asegurando una entrega eficiente y confiable mediante nuestro propio sistema de transporte.\nCon entusiasmo y optimismo, aspiramos a acompañar y crecer junto a nuestros clientes y proveedores, fortaleciendo nuestras relaciones y perfeccionando continuamente nuestros servicios.";
 
     return (
         <div className="contenedorPrincipalQuienesSomos">
-            <div className="fondoContainer">
-                <div className="logoQuienesSomosContainer">
-                    <img className="logoQuienesSomos" src={CAquienesSomos} />
-                </div>
-                <div className="imagenQuienesSomosContainer">
-                    <img className="imagenQuienesSomos" src={imagenQuienesSomos} />
-                </div>
-            </div>
             <div className="textoQuienesSomos">
                 <div className="tituloYPoligono">
                     <div className="poligono" />
@@ -26,6 +18,9 @@ export default function QuienesSomos() {
                         <div key={index}>{line}</div>
                     ))}
                 </div>
+            </div>
+            <div className="fondoContainer">
+                <img className="imagenQuienesSomos" src={imagenQuienesSomos} />
             </div>
         </div>
     );
