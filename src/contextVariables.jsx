ea3cafdca3d917705c0b11739bf514a4164d2989
MrPantufla <juanmapizzioo@gmail.com>
2024-08-05 07:40:55 -0300

    catelCliente movido de contexto

--- src/contextVariables.jsx
+++ src/contextVariables.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext } from 'react';
+import { useState } from 'react';
 
 const VariablesContext = createContext();
 
@@ -10,9 +11,13 @@ function VariablesProvider({ children }) {
     const backend = "http://localhost:8080";
     //const backend = "https://backend-calvo-415917.rj.r.appspot.com";
 
+    const [mostrarCartelCliente, setMostrarCartelCliente] = useState(false);
+
     return (
         <VariablesContext.Provider value={{
-            backend
+            backend,
+            mostrarCartelCliente,
+            setMostrarCartelCliente
         }}>
             {children}
         </VariablesContext.Provider>
