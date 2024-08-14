03ef9fc3b158e1f43a76479cb4c7df15a3700e0f
MrPantufla <juanmapizzioo@gmail.com>
2024-08-14 18:55:42 -0300

    Software creado y terminado

---
+++ src/Ventas/FiltrosYProductos/Software/imagenGrandeSoftware.jsx
@@ -0,0 +1,111 @@
+import { useTienda } from "../../../contextTienda";
+import { useEffect, useState } from "react";
+
+export default function ImagenSoftwareGrande() {
+    const [startX, setStartX] = useState(0);
+
+    const {
+        imagenSoftwareSeleccionada,
+        setImagenSoftwareSeleccionada,
+        isMobile
+    } = useTienda();
+
+    const clickParteInterna = (e) => {
+        e.stopPropagation();
+    }
+
+    const anterior = (e) => {
+        if (e) e.stopPropagation(); // Solo se ejecuta si 'e' está definido
+
+        if (imagenSoftwareSeleccionada > 1) {
+            setImagenSoftwareSeleccionada(imagenSoftwareSeleccionada - 1);
+        } else {
+            setImagenSoftwareSeleccionada(3);
+        }
+    }
+
+    const siguiente = (e) => {
+        if (e) e.stopPropagation(); // Solo se ejecuta si 'e' está definido
+
+        if (imagenSoftwareSeleccionada < 3) {
+            setImagenSoftwareSeleccionada(imagenSoftwareSeleccionada + 1);
+        } else {
+            setImagenSoftwareSeleccionada(1);
+        }
+    }
+
+    const handleTouchStart = (e) => {
+        setStartX(e.touches[0].clientX);
+    };
+
+    useEffect(() => {
+        if (isMobile) {
+            const handleDocumentTouchMove = (e) => {
+                const currentX = e.touches[0].clientX;
+                const diffX = currentX - startX;
+
+                if (diffX < -100) { // Verifica si el movimiento es de derecha a izquierda
+                    if (imagenSoftwareSeleccionada != null) {
+                        siguiente();
+                    }
+                } else if (diffX > 100) { // Verifica si el movimiento es de izquierda a derecha
+                    if (imagenSoftwareSeleccionada != null) {
+                        anterior();
+                    }
+                }
+            };
+
+            document.addEventListener('touchstart', handleTouchStart);
+            document.addEventListener('touchmove', handleDocumentTouchMove);
+
+            return () => {
+                document.removeEventListener('touchstart', handleTouchStart);
+                document.removeEventListener('touchmove', handleDocumentTouchMove);
+            };
+        }
+    }, [startX]);
+
+    useEffect(() => {
+        const handleKeyDown = (e) => {
+            if (imagenSoftwareSeleccionada != null) {
+                if (e.keyCode === 37) {
+                    anterior();
+                } else if (e.keyCode === 39) {
+                    siguiente();
+                } else if (e.keyCode === 27) {
+                    setImagenSoftwareSeleccionada(null);
+                }
+            }
+        };
+
+        document.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [imagenSoftwareSeleccionada]);
+
+    return (
+        <div className="imagenGrandeSoftware" onClick={() => setImagenSoftwareSeleccionada(null)}>
+            {!isMobile &&
+                <button className="anteriorProductoGrande" onClick={anterior}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="4rem" height="4rem" fill="white" className="bi bi-arrow-left-square-fill" viewBox="0 0 16 16">
+                        <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1" />
+                    </svg>
+                </button>
+            }
+
+            <div className="parteInternaImagenGrandeSoftware" onClick={clickParteInterna}>
+                <img src={`https://storage.googleapis.com/backend-calvo-415917.appspot.com/imagenesSoftware/winmaker${imagenSoftwareSeleccionada}.webp`} alt="Imagen winmaker grande" />
+            </div>
+
+            {!isMobile &&
+                <button className="siguienteProductoGrande" onClick={siguiente}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="4rem" height="4rem" fill="white" className="bi bi-arrow-right-square-fill" viewBox="0 0 16 16">
+                        <path d="M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1" />
+                    </svg>
+                </button>
+            }
+        </div>
+    );
+}
---
+++ src/Ventas/FiltrosYProductos/Software/software.jsx
@@ -0,0 +1,62 @@
+import './software.css';
+import { useTienda } from '../../../contextTienda';
+
+export default function Software() {
+
+    const {
+        isMobile,
+        setImagenSoftwareSeleccionada
+    } = useTienda();
+
+    return (
+        <div className="contenedorPrincipalSoftware">
+            <div className="headSoftware">
+                <img src="https://storage.googleapis.com/backend-calvo-415917.appspot.com/imagenesSoftware/logo.webp"></img>
+                <h1>WinMaker 10: La Solución Integral para Cotización de Aberturas de Aluminio</h1>
+            </div>
+            <p>WinMaker es la herramienta definitiva para simplificar el proceso de cotización de aberturas de aluminio. Diseñado específicamente para empresas del sector de la construcción y fabricantes de aberturas. Ofrece una solución integral que agiliza y mejora la precisión en la generación de presupuestos para proyectos de construcción.<br /></p>
+            <br />
+            <h2>Características Principales:</h2>
+            <p>
+                <span>1.    Interfaz Intuitiva:</span> Con una interfaz fácil de usar, permite a los usuarios ingresar medidas, especificaciones técnicas y detalles de materiales de manera rápida y sencilla.<br />
+                <span>2.	Generación Automática de Presupuestos:</span> Con solo unos pocos clics, genera presupuestos detallados y precisos basados en la información ingresada, ahorrando tiempo y minimizando errores.<br />
+                <span>3.	Personalización Flexible:</span> Adaptado a las necesidades específicas de cada proyecto, ofrece opciones de personalización para ajustarse a diferentes tipos de aberturas, materiales y accesorios.<br />
+                <span>4.	Gestión de Clientes y Proyectos:</span> Con herramientas integradas de gestión de clientes y proyectos, permite un seguimiento eficiente de las cotizaciones y la comunicación con los clientes.<br />
+                <span>5.	Compatibilidad con otros formatos:</span> Los presupuestos generados por WinMaker pueden exportarse fácilmente a formatos como PDF o Excel, facilitando su envío y presentación a los clientes.<br />
+                <br />
+            </p>
+
+            <div className='imagenesSoftwareContainer'>
+                <img src='https://storage.googleapis.com/backend-calvo-415917.appspot.com/imagenesSoftware/winmaker1.webp' onClick={() => setImagenSoftwareSeleccionada(1)} alt="imagen de winmaker"/>
+                <img src='https://storage.googleapis.com/backend-calvo-415917.appspot.com/imagenesSoftware/winmaker2.webp' onClick={() => setImagenSoftwareSeleccionada(2)} alt="imagen de winmaker"/>
+                <img src='https://storage.googleapis.com/backend-calvo-415917.appspot.com/imagenesSoftware/winmaker3.webp' onClick={() => setImagenSoftwareSeleccionada(3)} alt="imagen de winmaker"/>
+            </div>
+
+
+            <br />
+            <h2>Beneficios:</h2>
+            <p>
+                <span>• Ahorro de Tiempo:</span> el proceso de cotización se vuelve más rápido y eficiente, permitiendo a las empresas dedicar más tiempo a actividades clave de negocio.<br />
+                <span>• Precisión en la Estimación de Costos:</span> La generación automática de presupuestos garantiza una mayor precisión en la estimación de costos, reduciendo el riesgo de pérdidas financieras.<br />
+                <span>• Mejora de la Productividad:</span> Al simplificar y automatizar tareas administrativas, aumenta la productividad del equipo, permitiéndoles enfocarse en otros proyectos.<br />
+                <span>• Profesionalismo y Presentación de Calidad:</span> Los presupuestos tienen un aspecto profesional y detallado, lo que ayuda a transmitir una imagen de calidad y confianza ante los clientes.<br />
+                <br />
+                Descubrí cómo WinMaker puede transformar tu proceso de cotización ahorrando tiempo y dinero. ¡Contactanos hoy mismo para obtener más información!
+            </p>
+
+            <div className="contenedorBotonConsultarSoftware">
+                <a
+                    className="botonEnviarConsultaSoftware"
+                    href={isMobile ?
+                        ('https://wa.me/5493456475294?text=Consulta%20sobre%20WinMaker:%20')
+                        :
+                        ('https://web.whatsapp.com/send?phone=+5493456475294&text=Consulta%20sobre%20WinMaker:%20')
+                    }
+                    target='blank'
+                >
+                    CONSULTAR
+                </a>
+            </div>
+        </div>
+    );
+}
