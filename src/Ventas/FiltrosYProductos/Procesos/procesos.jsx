1f6f929ac2ec62fba125c660aa46d7827e36572c
MrPantufla <juanmapizzioo@gmail.com>
2024-08-20 16:17:37 -0300

    Link a whatsapp arreglado

--- src/Ventas/FiltrosYProductos/Procesos/procesos.jsx
+++ src/Ventas/FiltrosYProductos/Procesos/procesos.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from 'react-router-dom';
 export default function Procesos(args) {
 
     const navigate = useNavigate();
-    
+
     const {
         tipoProceso,
         setTipoProceso,
@@ -17,7 +17,8 @@ export default function Procesos(args) {
         setStipoProceso,
         acabado,
         setAcabado,
-        isTablet
+        isTablet,
+        isMobile
     } = useTienda();
 
     const {
@@ -37,21 +38,6 @@ export default function Procesos(args) {
             setTipoProceso(null);
         }
     }
-
-    const handleNavigateContacto = () => {
-        navigate('/', { replace: true });
-    
-        setTimeout(() => {
-          const contactoElement = document.getElementById('nuestrosHorarios');
-          if (contactoElement) {
-            contactoElement.scrollIntoView({
-              behavior: 'smooth',
-              block: 'start',
-              inline: 'nearest'
-            });
-          }
-        }, 100);
-      };
 
     return (
         <div className="contenedorPrincipalProcesos">
@@ -74,34 +60,40 @@ export default function Procesos(args) {
                 (<div className="row rowProductos">
                     {stipoProceso == null ?
                         <>
-                        {tipoProceso == 'pinturas' && (
-                            <h1 className="textoComunicateConNosotros textoProcesos">
-                                <span style={{ color: 'white' }}>¡IMPORTANTE!</span> Para realizar encargos de colores que no se listan a continuación,{' '}
-                                <span style={{ color: 'rgb(0, 60, 255)', cursor: 'pointer', textDecoration: 'underline' }} onClick={() => handleNavigateContacto()}>
-                                    comunicate con nosotros
-                                </span>
-                            </h1>
-                        )}
-                        {Object.values(procesos).map((proceso) => {
-                            if (!productosEliminados.includes(proceso.id)) {
-                                const colorCorregido = (proceso.color).replace(/\s+/g, '-');
-                                if (tipoProceso == 'anodizados' && proceso.rubro === 88) {
-                                    return (
-                                        <div key={proceso.id} className="col-12 col-md-6 col-lg-4 producto proceso">
-                                            <CardProcesos colorCorregido={colorCorregido} proceso={proceso} />
-                                        </div>
-                                    );
-                                } else if (tipoProceso == 'pinturas' && proceso.rubro !== 88 && proceso.rubro != 89) {
-                                    return (
-                                        <div key={proceso.id} className="col-12 col-md-6 col-lg-4 producto proceso">
-                                            <CardProcesos colorCorregido={colorCorregido} proceso={proceso} />
-                                        </div>
-                                    );
+                            {tipoProceso == 'pinturas' && (
+                                <h1 className="textoComunicateConNosotros textoProcesos">
+                                    <span style={{ color: 'white' }}>¡IMPORTANTE!</span> Para realizar encargos de colores que no se listan a continuación,{' '}
+                                    <a href={isMobile ?
+                                        (`https://wa.me/5493456475294`)
+                                        :
+                                        (`https://web.whatsapp.com/send?phone=+5493456475294`)
+                                    }
+                                        target='blank'
+                                        style={{ color: 'rgb(0, 60, 255)', cursor: 'pointer', textDecoration: 'underline' }}>
+                                        comunicate con nosotros
+                                    </a>
+                                </h1>
+                            )}
+                            {Object.values(procesos).map((proceso) => {
+                                if (!productosEliminados.includes(proceso.id)) {
+                                    const colorCorregido = (proceso.color).replace(/\s+/g, '-');
+                                    if (tipoProceso == 'anodizados' && proceso.rubro === 88) {
+                                        return (
+                                            <div key={proceso.id} className="col-12 col-md-6 col-lg-4 producto proceso">
+                                                <CardProcesos colorCorregido={colorCorregido} proceso={proceso} />
+                                            </div>
+                                        );
+                                    } else if (tipoProceso == 'pinturas' && proceso.rubro !== 88 && proceso.rubro != 89) {
+                                        return (
+                                            <div key={proceso.id} className="col-12 col-md-6 col-lg-4 producto proceso">
+                                                <CardProcesos colorCorregido={colorCorregido} proceso={proceso} />
+                                            </div>
+                                        );
+                                    }
+                                    return null;
                                 }
-                                return null;
-                            }
-                        })}
-                    </>
+                            })}
+                        </>
                         :
                         ((acabado == null && tipoProceso != 'pinturas') ?
                             <>
