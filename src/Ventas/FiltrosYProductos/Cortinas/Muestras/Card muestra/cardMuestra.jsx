9c9f699f566924a6a24eccf7ce8413575856a6ad
MrPantufla <juanmapizzioo@gmail.com>
2024-09-06 11:15:56 -0300

    Muestras de cortinas con zoom

--- src/Ventas/FiltrosYProductos/Cortinas/Muestras/Card muestra/cardMuestra.jsx
+++ src/Ventas/FiltrosYProductos/Cortinas/Muestras/Card muestra/cardMuestra.jsx
@@ -1,10 +1,40 @@
+import React, { useRef } from 'react';
 import './cardMuestra.css';
 
-export default function CardMuestra(args){
-    return(
+export default function CardMuestra(args) {
+    const imgRef = useRef(null);
+
+    let isMobile = window.innerWidth <= 767;
+
+    const handleMouseMove = (e) => {
+        if (imgRef.current) {
+            const img = imgRef.current;
+            const rect = img.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            // Ajusta la posición del desplazamiento en función de la posición del mouse
+            // Convertir las coordenadas del mouse a un porcentaje dentro del contenedor
+            const transformOriginX = ((x / rect.width) * 100); // Ajuste para centrar el zoom
+            const transformOriginY = ((y / rect.height) * 100); // Ajuste para centrar el zoom
+
+            img.style.transformOrigin = `${transformOriginX}% ${transformOriginY}%`;
+        }
+    };
+
+    return (
         <div className="contenedorPrincipalCardMuestra">
             <p>{args.titulo}</p>
-            <img src={args.muestra}/>
+            <div
+                className="contenedorImagen"
+                onMouseMove={handleMouseMove}
+            >
+                <img 
+                    src={args.muestra} 
+                    alt={args.titulo} 
+                    ref={imgRef}
+                />
+            </div>
         </div>
     );
 }
