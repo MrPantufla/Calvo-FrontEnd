d8cbf9715119a9fddac7f83d06a1f3922f44aff8
MrPantufla <juanmapizzioo@gmail.com>
2024-09-19 16:52:23 -0300

    atrasProcesos movido por problemas de superposicion

--- src/Ventas/ventas.jsx
+++ src/Ventas/ventas.jsx
@@ -18,7 +18,28 @@ export default function Ventas() {
     mostrarCartelError
   } = useCarrito();
 
+  const {
+    setStipoProceso,
+    setAcabado,
+    acabado,
+    tipoProceso,
+    stipoProceso,
+    setTipoProceso
+  } = useTienda();
+
   const { mostrarPagos } = useTienda();
+
+  const atrasProcesos = () => {
+    if (acabado != null && tipoProceso == 'anodizados') {
+      setAcabado(null);
+    }
+    else if (stipoProceso != null) {
+      setStipoProceso(null);
+    }
+    else if (tipoProceso != null) {
+      setTipoProceso(null);
+    }
+  }
 
   return (
     <>
@@ -31,6 +52,11 @@ export default function Ventas() {
         {mostrarCartelError && <CartelError />}
         {compraRealizadaAbierto && <PedidoRealizado />}
         {mostrarPagos && <Pagos />}
+        {tipoProceso && <button className="atrasProcesos" onClick={() => atrasProcesos()}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-90deg-left" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708z" />
+          </svg>
+        </button>}
         <Footer />
       </ProviderCortinas>
     </>
