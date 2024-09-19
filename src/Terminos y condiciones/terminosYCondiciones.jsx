2ea27d1bb525402d063bb4f6b89f00c83fd49f38
MrPantufla <juanmapizzioo@gmail.com>
2024-09-19 16:53:48 -0300

    actualizacion de TyC

--- src/Terminos y condiciones/terminosYCondiciones.jsx
+++ src/Terminos y condiciones/terminosYCondiciones.jsx
@@ -10,29 +10,31 @@ export default function TerminosYCondiciones() {
 
                 2- La empresa se reserva el derecho de utilizar los datos brindados por el usuario para establecer comunicaciones vía email, WhatsApp o llamadas telefónicas ante cualquier problema o necesidad de consulta.<br/>
 
-                3- Los datos ingresados por el cliente serán almacenados en una base de datos tanto para mejorar la experiencia del cliente en el sitio web como para uso interno de la empresa, comprometiéndose esta a jamás facilitar estos datos a cualquier ente o persona ajenos a la misma, así como también a mantener un estricto sistema de encriptado de datos sensibles para garantizar la inaccesibilidad a los mismos por parte de cualquier persona, tanto dentro como fuera de la empresa.<br/>
+                3- La empresa se reserva el derecho a realizar un seguimiento de la actividad del uso del sitio web de los usuarios con fines de mejorar el servicio.<br/>
 
-                4- La empresa se reserva el derecho de cancelar pedidos, dar de alta o de baja cuentas de usuario y realizar cualquier modificación al sitio web.<br/>
+                4- Los datos ingresados por el cliente serán almacenados en una base de datos tanto para mejorar la experiencia del cliente en el sitio web como para uso interno de la empresa, comprometiéndose esta a jamás facilitar estos datos a cualquier ente o persona ajenos a la misma, así como también a mantener un estricto sistema de encriptado de datos sensibles para garantizar la inaccesibilidad a los mismos por parte de cualquier persona, tanto dentro como fuera de la empresa.<br/>
 
-                5- La empresa se reserva el derecho a definir el medio de entrega de los productos que sean solicitados por entrega a domicilio, acorde a la disponibilidad del servicio.<br/>
+                5- La empresa se reserva el derecho de cancelar pedidos, dar de alta o de baja cuentas de usuario y realizar cualquier modificación al sitio web.<br/>
 
-                6- Las imágenes, representaciones y descripciones de los productos expuestos son ilustrativas y no necesariamente representan con exactitud al producto final.<br/>
+                6- La empresa se reserva el derecho a definir el medio de entrega de los productos que sean solicitados por entrega a domicilio, acorde a la disponibilidad del servicio.<br/>
 
-                7- Las imágenes expuestas son de uso y propiedad de la empresa; cualquier uso no autorizado de las mismas puede derivar en consecuencias legales de acuerdo al régimen legal de la propiedad intelectual.<br/>
+                7- Las imágenes, representaciones y descripciones de los productos expuestos son ilustrativas y no necesariamente representan con exactitud al producto final.<br/>
 
-                8- El usuario acepta el uso y almacenamiento de cookies con el objetivo de garantizar el correcto funcionamiento del sitio web, las cuales constan de un sistema de encriptado de información que las protege de ciberataques u otros mecanismos que tengan como fin al sustrato de datos.<br/>
+                8- Las imágenes expuestas son de uso y propiedad de la empresa; cualquier uso no autorizado de las mismas puede derivar en consecuencias legales de acuerdo al régimen legal de la propiedad intelectual.<br/>
 
-                9- En caso de que el usuario desee cancelar una compra luego de efectuado el pago, se procederá a realizar una nota de crédito. Dicho usuario tendrá el derecho de utilizar el dinero transferido como saldo positivo para una próxima compra, más no se realizará la devolución del dinero.<br/>
+                9- El usuario acepta el uso y almacenamiento de cookies con el objetivo de garantizar el correcto funcionamiento del sitio web, las cuales constan de un sistema de encriptado de información que las protege de ciberataques u otros mecanismos que tengan como fin al sustrato de datos.<br/>
 
-                10- El precio listado de los perfiles es aproximado en base al peso estimativo de los mismos, pudiendo este variar una vez pesado el producto al momento de armar el pedido.<br/>
+                10- En caso de que el usuario desee cancelar una compra luego de efectuado el pago, se procederá a realizar una nota de crédito. Dicho usuario tendrá el derecho de utilizar el dinero transferido como saldo positivo para una próxima compra, más no se realizará la devolución del dinero.<br/>
 
-                11- Los productos se encuentran sujetos a disponibilidad de la empresa, pudiendo darse el caso en el que se concrete la venta de un artículo falto de stock. La empresa se compromete en dicho caso a establecer una comunicación con el usuario por alguno de los medios mencionados en el punto número 2 para solventar el problema.<br/>
+                11- El precio listado de los perfiles es aproximado en base al peso estimativo de los mismos, pudiendo este variar una vez pesado el producto al momento de armar el pedido.<br/>
+
+                12- Los productos se encuentran sujetos a disponibilidad de la empresa, pudiendo darse el caso en el que se concrete la venta de un artículo falto de stock. La empresa se compromete en dicho caso a establecer una comunicación con el usuario por alguno de los medios mencionados en el punto número 2 para solventar el problema.<br/>
  
-                12- Los precios listados pueden variar sin previo aviso. <br/>
+                13- Los precios listados pueden variar sin previo aviso. <br/>
 
-                13- Para concretar un pedido de perfiles con procesos, se debe abonar una seña del 50% del valor estimativo del mismo. Posterior a la realización del pedido, una persona de ventas se comunicará con el cliente por medio de uno de los medios mencionados en el punto número 2 para concretar el medio por el cual realizar dicha transacción e informar el plazo de demora del pedido.<br/>
+                14- Para concretar un pedido de perfiles con procesos, se debe abonar una seña del 50% del valor estimativo del mismo. Posterior a la realización del pedido, una persona de ventas se comunicará con el cliente por medio de uno de los medios mencionados en el punto número 2 para concretar el medio por el cual realizar dicha transacción e informar el plazo de demora del pedido.<br/>
 
-                14- Este contrato se rige bajo la vigente legislación de la República Argentina, quedando cualquier conflicto relacionado con este contrato o el uso del sitio web subordinado a las leyes que rijan en el momento de producirse el percance.
+                15- Este contrato se rige bajo la vigente legislación de la República Argentina, quedando cualquier conflicto relacionado con este contrato o el uso del sitio web subordinado a las leyes que rijan en el momento de producirse el percance.
             </p>
         </div>
     );
