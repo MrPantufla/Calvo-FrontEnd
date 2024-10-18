a321badf1e5093dba5b0372c2655289d7455309e
MrPantufla <juanmapizzioo@gmail.com>
2024-10-18 15:39:22 -0300

    Boton cerrar y escape

--- src/Ventas/Confirmar compra/confirmarCompra.jsx
+++ src/Ventas/Confirmar compra/confirmarCompra.jsx
@@ -16,8 +16,18 @@ export default function ConfirmarCompra(args) {
         setTipoEnvio,
         medioEnvio,
         metodoPago,
-        metodoFacturacion
+        metodoFacturacion,
+        keyDownEnter,
+        setKeyDownEnter
     } = useFinalizarCompra();
+
+    const {
+        setMostrarEnvios,
+        setMostrarPagos,
+        setMostrarFacturacion,
+        setMostrarFinalizarPedido,
+        setMostrarConfirmarCompra
+    } = useVariables();
 
     const {
         state
@@ -58,10 +68,51 @@ export default function ConfirmarCompra(args) {
     useEffect(() => {
         obtenerSucursal();
 
-        if(state.userInfo && !state.userInfo.cliente){
+        if (state.userInfo && !state.userInfo.cliente) {
             setTipoEnvio('correo')
         }
     }, [])
+
+    const escConfirmarCompra = (e) => {
+        if (e.key === 'Escape') {
+            setMostrarConfirmarCompra(false);
+            setMostrarEnvios(false);
+            setMostrarPagos(false);
+            setMostrarFacturacion(false);
+            setMostrarFinalizarPedido(false);
+        }
+    };
+
+    useEffect(() => {
+        // Agregar el event listener al montar el componente
+        window.addEventListener('keydown', escConfirmarCompra);
+
+        // Limpiar el event listener al desmontar el componente
+        return () => {
+            window.removeEventListener('keydown', escConfirmarCompra);
+        };
+    }, []);
+
+    /*ESTO QUEDA PARA AGREGAR ENVIAR FORMULARIOS CON ENTER
+    const enterConfirmarCompra = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            console.log(keyDownEnter)
+            if (typeof keyDownEnter === 'function') {
+                keyDownEnter();
+            }
+        }
+    };
+
+    useEffect(() => {
+        // Agregar el event listener al montar el componente
+        window.addEventListener('keydown', enterConfirmarCompra);
+
+        // Limpiar el event listener al desmontar el componente
+        return () => {
+            window.removeEventListener('keydown', enterConfirmarCompra);
+        };
+    }, []);*/
 
     return (
         <div className="contenedoresConfirmarCompra">
@@ -73,22 +124,27 @@ export default function ConfirmarCompra(args) {
                         </svg>
                     </button>
                     <p>{args.titulo}</p>
+                    <button onClick={() => { setMostrarEnvios(false); setMostrarPagos(false); setMostrarFacturacion(false); setMostrarFinalizarPedido(false); setMostrarConfirmarCompra(false) }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="white" className="bi bi-x" viewBox="0 0 16 16">
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                        </svg>
+                    </button>
                 </div>
                 <div className="botonesOpcionesConfirmarCompra">
                     {args.componentesArray.map((seccion, index) => {
 
                         return (
-                            <button onClick={() => {toggleOpcion(seccion); setErrorMessage('')}} className={(medioEnvio == seccion.comparador || metodoPago == seccion.comparador || metodoFacturacion == seccion.comparador) ? 'active' : ''} key={"boton" + index}>{seccion.nombre}</button>
+                            <button onClick={() => { toggleOpcion(seccion); setErrorMessage('') }} className={(medioEnvio == seccion.comparador || metodoPago == seccion.comparador || metodoFacturacion == seccion.comparador) ? 'active' : ''} key={"boton" + index}>{seccion.nombre}</button>
                         )
                     })}
                 </div>
 
                 {errorMessage !== '' && (
                     <p className="errorFinalizarCompra">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="1.3rem" height="1.3rem" fill="var(--colorRojo)" className="bi bi-exclamation-diamond-fill svgErrorFormulario" viewBox="0 0 16 16">
-                        <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-                    </svg>
-                    {errorMessage}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1.3rem" height="1.3rem" fill="var(--colorRojo)" className="bi bi-exclamation-diamond-fill svgErrorFormulario" viewBox="0 0 16 16">
+                            <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+                        </svg>
+                        {errorMessage}
                     </p>
                 )}
 
