d54a09d5be1d2cbffe6ed0601ec2beac2caa8a2a
MrPantufla <juanmapizzioo@gmail.com>
2024-12-12 09:36:15 -0300

    Inputs mejorados

--- src/Ventas/FiltrosYProductos/Card Producto/cardProducto.jsx
+++ src/Ventas/FiltrosYProductos/Card Producto/cardProducto.jsx
@@ -350,7 +350,7 @@ export default function CardProducto(args) {
                 </svg>
               </button>
 
-              <input className="inputDescuento" defaultValue={ofertaEncontrada ? ofertaEncontrada.descuento : null} onChange={(e) => setDescuento(e.target.value)} />
+              <input type="number" className="inputDescuento" defaultValue={ofertaEncontrada ? ofertaEncontrada.descuento : null} onChange={(e) => setDescuento(e.target.value)} />
               <button className={`ofertarProducto ${ofertaEncontrada && 'active'}`} onClick={() => ((ofertasSelected || descuento <= 0) ? eliminarOferta(id) : ofertarProducto(id, descuento))}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="2rem" height="2rem" fill="currentColor" className="bi bi-receipt-cutoff" viewBox="0 0 16 16">
                   <path d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5M11.5 4a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z" />
