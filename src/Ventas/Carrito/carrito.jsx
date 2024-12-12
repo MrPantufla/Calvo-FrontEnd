1483a68ae2d0128cf84d65b953c3cbaab8c7cef3
MrPantufla <juanmapizzioo@gmail.com>
2024-12-12 09:43:10 -0300

    Presupuestos vacíos arreglados

--- src/Ventas/Carrito/carrito.jsx
+++ src/Ventas/Carrito/carrito.jsx
@@ -50,7 +50,8 @@ export default function Carrito(args) {
     setMostrarFacturacion,
     setMostrarConfirmarCompra,
     setMostrarCartelCliente,
-    mostrarCartelCliente
+    mostrarCartelCliente,
+    setMostrarPagos
   } = useVariables();
 
   const { setFavoritosAbierto } = useFavoritos();
@@ -71,7 +72,6 @@ export default function Carrito(args) {
   const cantidadInputRef = useRef(null);
   const [sugerenciaColor, setSugerenciaColor] = useState('');
   const [inputFocused, setInputFocused] = useState('');
-  const [carritoHeight, setCarritoHeight] = useState(0);
   const [mostrarHint, setMostrarHint] = useState(false);
   const pdfCarritoRef = useRef(null);
 
@@ -285,7 +285,9 @@ export default function Carrito(args) {
   const precioParaMostrarString = calcularTotal ? parseInt(calcularTotal(elementos, false)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") : 0;
   const precioParaMostrarStringDescuento = calcularTotal ? (parseInt(calcularTotal(elementos, true))).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") : 0;
 
-  const enviarPresupuesto = async () => {
+  const enviarPresupuesto = async (e) => {
+    e.preventDefault();
+
     let tokenParaEnviar = Cookies.get('jwtToken');
 
     if (tokenParaEnviar == undefined) {
@@ -334,12 +336,12 @@ export default function Carrito(args) {
         <div className="periferiaCarrito">
           <div className="tituloYHintCarrito">
             <PdfCarrito ref={pdfCarritoRef} />
-            <div className={`botonDescargarPresupuesto ${elementos.length < 1 && 'disabled'}`} onClick={() => { generarPdf(); enviarPresupuesto() }}>
+            <button disabled={elementos.length < 1} className={`botonDescargarPresupuesto ${elementos.length < 1 && 'disabled'}`} onClick={(e) => { generarPdf(); enviarPresupuesto(e) }}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-download" viewBox="0 0 16 16">
                 <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5" />
                 <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708z" />
               </svg>
-            </div>
+            </button>
             <p className="tituloCarrito">CARRITO - COMPRA RÁPIDA</p>
             <div className="botonHintCarrito" onClick={() => setMostrarHint(!mostrarHint)}>
               {mostrarHint ? (<p>X</p>) : (<p>?</p>)}
@@ -430,8 +432,9 @@ export default function Carrito(args) {
                   disabled={!elementos.length > 0}
                   onClick={() => {
                     setPrecioTotal(calcularTotal(elementos));
-                    setMostrarFacturacion(true);
+                    //setMostrarFacturacion(true);
                     setMostrarConfirmarCompra(true);
+                    setMostrarPagos(true);
                     //setMostrarEnvios(true);
                   }}
                 >
