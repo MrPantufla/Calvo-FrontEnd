70c550319d5c1384b4aea5e3fee7be26143cde52
MrPantufla <juanmapizzioo@gmail.com>
2024-12-12 09:43:43 -0300

    Modificacion completa de inscripto

--- src/Ventas/Confirmar compra/Facturacion/inscripto.jsx
+++ src/Ventas/Confirmar compra/Facturacion/inscripto.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useFinalizarCompra } from "../../../contextFinalizarCompra";
 import { useCarrito } from "../../../contextCarrito";
 import { useVariables } from "../../../contextVariables";
@@ -10,12 +10,19 @@ export default function Inscripto() {
         limpiarCarrito,
         setCarritoAbierto
     } = useCarrito();
-    
+
     const {
         setErrorMessage,
-        cuit, 
+        cuit,
         setCuit,
-        almacenarFacturacion
+        almacenarFacturacion,
+        metodoPago,
+        dni,
+        setDatosPedido,
+        primerosDigitos,
+        setPrimerosDigitos,
+        ultimoDigito,
+        setUltimoDigito
     } = useFinalizarCompra();
 
     const {
@@ -23,7 +30,12 @@ export default function Inscripto() {
         setMostrarFinalizarPedido
     } = useVariables();
 
-    const confirmar = (e) =>{
+    const [inputsFocus, setInputsFocus] = useState(false);
+
+    const primerosDigitosRef = useRef(null);
+    const ultimoDigitoRef = useRef(null);
+
+    const confirmar = (e) => {
         e.preventDefault();
 
         const numerosRegex = /^[0-9]+$/;
@@ -42,26 +54,102 @@ export default function Inscripto() {
             return;
         }
 
-        let datosPedido = 'Facturar a ' + cuit;
-        
+        setDatosPedido('Facturar a ' + cuit);
+
         setMostrarFacturacion(false);
-        //setMostrarFinalizarPedido(true);
+        setMostrarFinalizarPedido(true);
         almacenarFacturacion();
-        limpiarCarrito();
-        confirmarCompra(datosPedido)
+        //limpiarCarrito();
+        //confirmarCompra(datosPedido)
+        setErrorMessage('');
+    }
+
+    console.log(cuit)
+
+    const keyDownCuit = (slot, valor) => {
+        return (event) => {
+            let cursorPosition;
+            let endPosition;
+
+            if (slot == 0) {
+                cursorPosition = primerosDigitosRef.current.selectionStart;
+                endPosition = primerosDigitosRef.current.selectionEnd;
+            }
+            else {
+                cursorPosition = ultimoDigitoRef.current.selectionStart;
+                endPosition = ultimoDigitoRef.current.selectionEnd;
+            }
+
+            //Se verifica que no estemos seleccionando varios numeros, que no estemows en el primer input y que estemos en la posicion 0
+            if (event.key === 'Backspace' || event.key === 'ArrowLeft') {
+                if ((cursorPosition === endPosition) && (slot > 0) && (cursorPosition === 0 || valor.length === 0)) {
+                    event.preventDefault();
+                    const previousInput = primerosDigitosRef.current;
+                    previousInput.focus();
+
+                    previousInput.setSelectionRange(previousInput.value.length, previousInput.value.length); //Parar el cursor en el ultimo caracter
+                }
+            }
+            else if (event.key == 'ArrowRight') {
+                if ((slot < 1) && (cursorPosition === valor.length)) {
+                    const nextInput = ultimoDigitoRef.current;
+                    nextInput.focus();
+
+                    setTimeout(() => {
+                        nextInput.setSelectionRange(0, 0); // Posicionar el cursor al inicio del siguiente input
+                    }, 0);
+                }
+            }
+            else if(/^[0-9]$/.test(event.key)) {
+                if(slot == 0 && valor.length == 2){
+                    ultimoDigitoRef.current.focus()
+                }
+            }
+        }
     }
 
     return (
         <>
-            <div className="contenedorFormConfirmarCompra">
+            <div className="contenedorFormConfirmarCompra contenedorInscripto">
                 <div className="contenedorEntradaConfirmarCompra">
-                    <label htmlFor="cuit">CUIT</label>
-                    <input id="cuit"
-                        value={cuit}
-                        onChange={(e) => setCuit(e.target.value)}
-                        onFocus={() => setErrorMessage('')}
-                        type='number'
-                    />
+                    {metodoPago == 'tarjeta' ?
+                        (<>
+                            <label htmlFor="primerosDigitos">CUIT</label>
+                            <div className="contenedorCuitInscriptoTarjeta">
+                                <input
+                                    className={`${inputsFocus && 'focuseado'}`}
+                                    value={primerosDigitos}
+                                    onChange={(e) => {e.target.value.length >= 2 && ultimoDigitoRef.current.focus(); setPrimerosDigitos(e.target.value.replace(/[^0-9]/g, '')); setCuit(e.target.value + dni + ultimoDigito)}}
+                                    onFocus={() => setInputsFocus(true)}
+                                    onBlur={() => { inputsFocus && setInputsFocus(false) }}
+                                    inputMode="numeric"
+                                    maxLength="2"
+                                    id="primerosDigitos"
+                                    ref={primerosDigitosRef}
+                                    onKeyDown={(e) => keyDownCuit(0, primerosDigitos)(e)}
+                                />
+                                <p className={`${inputsFocus && 'focuseado'}`}>{dni}</p>
+                                <input
+                                    className={`${inputsFocus && 'focuseado'}`}
+                                    value={ultimoDigito}
+                                    onChange={(e) => {setUltimoDigito(e.target.value); setCuit(primerosDigitos + dni + e.target.value.replace(/[^0-9]/g, ''))}}
+                                    onFocus={() => setInputsFocus(true)}
+                                    onBlur={() => { inputsFocus && setInputsFocus(false) }}
+                                    inputMode="numeric"
+                                    maxLength="1"
+                                    ref={ultimoDigitoRef}
+                                    onKeyDown={(e) => keyDownCuit(1, ultimoDigito)(e)}
+                                />
+                            </div>
+                        </>)
+                        :
+                        (<input id="cuit"
+                            value={cuit}
+                            onChange={(e) => setCuit(e.target.value)}
+                            onFocus={() => setErrorMessage('')}
+                            inputMode="numeric"
+                        />)
+                    }
                 </div>
             </div>
             <div className="contenedorConfirmarBoton">
