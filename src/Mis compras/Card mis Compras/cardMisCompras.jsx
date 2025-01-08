fa26b7f1d631cf60b2ebe7ab25b6e5b777536ab2
MrPantufla <juanmapizzioo@gmail.com>
2025-01-08 09:34:10 -0300

    Descargar pedido implementado

--- src/Mis compras/Card mis Compras/cardMisCompras.jsx
+++ src/Mis compras/Card mis Compras/cardMisCompras.jsx
@@ -13,7 +13,8 @@ export default function CardMisCompras(args) {
     const {
         productosIndexado,
         productosSueltos,
-        marcasUnicas
+        marcasUnicas,
+        procesos
     } = useProductos();
 
     const {
@@ -50,7 +51,7 @@ export default function CardMisCompras(args) {
             }
 
             if (producto) {
-                return accumulator + (precioProvisional - (precioProvisional / 100 * (currentItem.descuento || 0 )));
+                return accumulator + (precioProvisional - (precioProvisional / 100 * (currentItem.descuento || 0)));
             } else {
                 return accumulator + ((currentItem.p_vta * currentItem.cantidad) - ((currentItem.p_vta * currentItem.cantidad) / 100 * (currentItem.descuento || 0)));
             }
@@ -62,7 +63,7 @@ export default function CardMisCompras(args) {
 
             let producto = productosIndexado[args.data[index].producto];
 
-            if(!producto){
+            if (!producto) {
                 producto = productosSueltos[args.data[index].producto];
             }
 
@@ -105,6 +106,56 @@ export default function CardMisCompras(args) {
         return marcaEnSet;
     });
 
+    let objetoCSV = [];
+
+    args.data.forEach((item) => {
+
+        let producto = null;
+
+        if (productosIndexado[item.producto]) {
+            producto = productosIndexado[item.producto];
+        } else if (productosSueltos[item.producto]) {
+            producto = productosSueltos[item.producto];
+        }
+
+        if (producto != null) {
+
+            objetoCSV.push(
+                {
+                    Código: (marcasUnicas.has(producto.marca) ? (producto.cod_orig) : (producto.cod_int)),
+                    Detalle: producto.detalle + (item.acabado != 0 ? (' - ' + procesos[item.acabado].detalle) : ''),
+                    Color: (item.proceso == 0 ? producto.color : (procesos[item.proceso].rubro == 88 ? (`Anodizado ${procesos[item.proceso].color}`) : (procesos[item.proceso].color))),
+                    Cantidad: item.cantidad
+                }
+            );
+        }
+    });
+
+    function generarCSV(datos) {
+        // Obtener encabezados desde las claves del primer objeto
+        const encabezados = Object.keys(datos[0]).join(";");
+
+        // Generar las filas de datos
+        const filas = datos.map(fila =>
+            Object.values(fila).map(valor => `"${valor}"`).join(";")
+        );
+
+        // Combinar encabezados y filas
+        return [encabezados, ...filas].join("\n");
+    }
+
+    const descargarArchivo = (nombreArchivo, contenido) => {
+        const BOM = "\uFEFF";
+        const blob = new Blob([BOM + contenido], { type: "text/csv;charset=utf-8;" });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", nombreArchivo);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
         <div className="contenedorPrincipalCardMisCompras" >
             <div className="headCardMisCompras">
@@ -133,9 +184,15 @@ export default function CardMisCompras(args) {
             <div className={`bodyCardMisCompras ${cardComprasAbierto == true && 'open'}`}>
                 <div className="productosHistorialContainer">
                     {Array.from({ length: args.data.length }).map((_, index) => (
-                        <ProductoHistorial key={index} id={args.data[index].producto} cantidad={args.data[index].cantidad} precio={args.data[index].p_vta} proceso={args.data[index].proceso} acabado={args.data[index].acabado} descuento={args.data[index].descuento || 0}/>
+                        <ProductoHistorial key={index} id={args.data[index].producto} cantidad={args.data[index].cantidad} precio={args.data[index].p_vta} proceso={args.data[index].proceso} acabado={args.data[index].acabado} descuento={args.data[index].descuento || 0} />
                     ))}
                 </div>
+                <button onClick={() => descargarArchivo(`pedidoCalvo${fechaFormateada}.csv`, generarCSV(objetoCSV))}>
+                    Descargar pedido <svg xmlns="http://www.w3.org/2000/svg" width="1.8rem" height="1.8rem" fill="currentColor" class="bi bi-file-earmark-excel" viewBox="0 0 16 16">
+                        <path d="M5.884 6.68a.5.5 0 1 0-.768.64L7.349 10l-2.233 2.68a.5.5 0 0 0 .768.64L8 10.781l2.116 2.54a.5.5 0 0 0 .768-.641L8.651 10l2.233-2.68a.5.5 0 0 0-.768-.64L8 9.219l-2.116-2.54z" />
+                        <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+                    </svg>
+                </button>
                 <div className="totalContainer">
                     <h2 className="precioViejo">
                         {`TOTAL${hayPerfil ? ' APROXIMADO' : ''}: $${precioParaMostrarString} `}
