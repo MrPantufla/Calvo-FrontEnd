345426413989faf58c274eb39eac1d68ddc3d15a
MrPantufla <juanmapizzioo@gmail.com>
2025-01-08 09:33:41 -0300

    Visibilidad de imagenes personalizada implementada

--- src/Principal/Body/Carrusel/carrusel.jsx
+++ src/Principal/Body/Carrusel/carrusel.jsx
@@ -9,6 +9,8 @@ import Cookies from 'js-cookie';
 export default function Carrusel() {
   const [listaImagenes, setListaImagenes] = useState([]);
   const [imagenesCargadas, setImagenesCargadas] = useState(false);
+  const [visibilidad, setVisibilidad] = useState([]);
+  const [primeraCarga, setPrimeraCarga] = useState(true);
 
   const { state } = useAuth();
 
@@ -115,44 +117,127 @@ export default function Carrusel() {
     obtenerImagenes();
   }, [backend]);
 
+  const cambiarVisibilidadDb = async (index, nuevaVisibilidad) => {
+
+    const itemEnviar = {
+      index: index,
+      visibilidad: nuevaVisibilidad
+    }
+
+    let tokenParaEnviar = Cookies.get('jwtToken');
+
+    if (tokenParaEnviar == undefined) {
+      tokenParaEnviar = null;
+    }
+
+    const response = await fetch(`${backend}/visibilidadCarrusel/post`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': tokenParaEnviar,
+      },
+      credentials: 'include',
+      body: JSON.stringify(itemEnviar),
+    });
+
+    if (response.ok) {
+      setVisibilidad((prevVisibilidad) => {
+        const nuevaVisibilidadArray = [...prevVisibilidad]; // Crear una copia del arreglo actual
+        nuevaVisibilidadArray[index - 1] = nuevaVisibilidad; // Asignar el nuevo valor al índice correspondiente
+        return nuevaVisibilidadArray; // Retornar el nuevo arreglo
+      });
+      console.log("Visibilidad cambiada con éxito")
+    }
+  }
+
+  useEffect(() => {
+    const asignarVisibilidades = async () => {
+      if (!primeraCarga) {
+        try {
+          const permisosDb = await obtenerListaVisilibilidad();
+
+          const arrayPermisos = listaImagenes.map((_, i) => {
+            const permiso = permisosDb.find(p => p.index == i + 1);
+            return permiso ? [permiso.visibilidad] : ["clientes"];
+          });
+
+          setVisibilidad(arrayPermisos);
+        } catch (error) {
+          console.error("Error al obtener permisos de visibilidad:", error);
+        }
+      } else {
+        setPrimeraCarga(false);
+      }
+    };
+
+    asignarVisibilidades();
+  }, [listaImagenes]);
+
+  const obtenerListaVisilibilidad = async () => {
+    try {
+      const response = await fetch(`${backend}/visibilidadCarrusel/get`);
+
+      if (!response.ok) {
+        throw new Error('Error al obtener la lista de visibilidades');
+      }
+
+      const data = await response.json();
+      return data;
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <div className="container">
+    <div className="contenedorPrincipalCarrusel">
       {imagenesCargadas ?
         (<Carousel interval={intervalo}>
           {
             listaImagenes.map((imageName, index) => (
-              index !== 0 && (
+              (index !== 0 && (visibilidad[index - 1]) == 'todos' || visibilidad[index - 1] == 'registrados' && state.userInfo || visibilidad[index - 1] == 'clientes' && state.userInfo && state.userInfo.cliente) && (
                 <Carousel.Item key={index}>
                   <img
                     className="d-block w-100"
                     src={imageName}
                     alt={`Slide ${index}`}
                   />
-                  {(state.userInfo && (state.userInfo.tipo_usuario == 'admin' || state.userInfo.tipo_usuario == 'colaborador')) && (
-                    <>
-                      <div className="divSubirArchivoCarrusel storageCarrusel">
-                        <label htmlFor="subirImagen" className="boton-personalizado">+</label>
-                        <input id="subirImagen" type="file" onChange={handleFileUpload} style={{ display: "none" }} accept=".png, .jpg, .jpeg, .svg" />
-                      </div>
 
-                      {listaImagenes.length > 2 && (
-                        <div className="divEliminarArchivoCarrusel storageCarrusel">
-                          <label htmlFor={`eliminarImagen_${index}`} className="boton-personalizado">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="4.5rem" height="4.5rem" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
-                              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                            </svg>
-                          </label>
-                          <input id={`eliminarImagen_${index}`} onClick={() => eliminarImagen(imageName)} style={{ display: "none" }} />
+                  {(state.userInfo && (state.userInfo.tipo_usuario == 'admin' || state.userInfo.tipo_usuario == 'colaborador')) &&
+                    (
+                      <>
+                        <div className="divSubirArchivoCarrusel storageCarrusel">
+                          <label htmlFor="subirImagen" className="boton-personalizado">+</label>
+                          <input id="subirImagen" type="file" onChange={handleFileUpload} style={{ display: "none" }} accept=".png, .jpg, .jpeg, .svg, .webp" />
                         </div>
-                      )}
-                    </>
-                  )}
+
+                        <div className="divPermisosCarrusel">
+                          <button className={visibilidad[index - 1] == 'todos' ? 'active' : ''} onClick={() => cambiarVisibilidadDb(index, 'todos')}>{}Todos</button>
+                          <button className={visibilidad[index - 1] == 'registrados' ? 'active' : ''} onClick={() => cambiarVisibilidadDb(index, 'registrados')}>Registrados</button>
+                          <button className={visibilidad[index - 1] == 'clientes' ? 'active' : ''} onClick={() => cambiarVisibilidadDb(index, 'clientes')}>Clientes</button>
+                        </div>
+
+                        {listaImagenes.length > 2 &&
+                          (
+                            <div className="divEliminarArchivoCarrusel storageCarrusel">
+                              <label htmlFor={`eliminarImagen_${index}`} className="boton-personalizado">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="4.5rem" height="4.5rem" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+                                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                                </svg>
+                              </label>
+                              <input id={`eliminarImagen_${index}`} onClick={() => eliminarImagen(imageName)} style={{ display: "none" }} />
+                            </div>
+                          )}
+                      </>
+                    )}
 
                 </Carousel.Item>
               )
             ))
+
           }
-        </Carousel>)
+        </Carousel>
+        )
         :
         (<div className="cargandoCarouselContainer">
           <div className="spinner-border cargandoCarousel" role="status">
